7462b406ead31016e8431d17bb20201d2401cd0c
Jinsong <jinsongliu@utexas.edu>
2019-09-05 14:26:16 -0500

    Cleaning tests folder  Changes to be committed: 	deleted:    museuq/solver/.benchmark.py.swp 	deleted:    tests/sparse_pce.py 	modified:   tests/test_basic.py 	deleted:    tests/test_gen_gauss_time_series.pdf

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,6 +11,27 @@ class BasicTestSuite(unittest.TestCase):
     def test_absolute_truth_and_meaning(self):
         assert True
 
+    def test_acfPsd(self):
+        ## refer to file test_acfPsd.py
+        pass
+
+    def test_gen_gauss_time_series(self):
+        ## refer to file  test_gen_gauss_time_series
+        pass
+
+    def test_sdof_var(self):
+        ## refer to file: test_sdof_var
+        pass
+
+    def test_poly5(self):
+        ## refer to file: test_poly5
+        pass
+
+    def test_solver(self):
+        ## refer to file: test_solver
+        pass
+
+
 
 if __name__ == '__main__':
     unittest.main()
