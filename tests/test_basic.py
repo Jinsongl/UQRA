66f207bffb5e6cfe4c3059c1d538db43580a2429
Jinsong <jinsongliu@utexas.edu>
2019-10-02 15:13:50 -0500

    test_basic modified for Gauss Quadrature and added Kvitebjorn testing  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   ../../tests/test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,9 +2,12 @@
 
 import context, museuq, unittest,warnings
 import numpy as np, chaospy as cp, os, sys
+import scipy.stats as stats
 from museuq.utilities import helpers as uqhelpers 
 from museuq.utilities import constants as const
+from museuq.utilities import dataIO as museuq_dataio
 from museuq.utilities.PowerSpectrum import PowerSpectrum
+from museuq.environment import Kvitebjorn as Kvitebjorn
 
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 sys.stdout  = museuq.utilities.classes.Logger()
@@ -17,48 +20,44 @@ class BasicTestSuite(unittest.TestCase):
         """
         https://keisan.casio.com/exec/system/1329114617
         """
-        print('========================TESTING: GAUSS QUADRATURE=======================')
 
-        quadrature_rule_to_test = ['leg', 'hem', 'lag', 'jacobi']
-        quadrature_dist_to_test = [cp.Uniform(-1,1), cp.Normal(), cp.Gamma(1,1), cp.Beta(1,1)]
-        #cp.Gamma() def: x**(a-1)*numpy.e**(-x) / special.gamma(a)
-    #Legendre-Gauss quadrature 
-        # n = 2
-            # i	weight - wi	abscissa - xi
-            # ___________________________________________________
-            # 1	1.0000000000000000	-0.5773502691896257
-            # 2	1.0000000000000000	0.5773502691896257
-        # n = 3
-            # ___________________________________________________
-            # i	weight - wi	abscissa - xi
-            # 1	0.8888888888888888	0.0000000000000000
-            # 2	0.5555555555555556	-0.7745966692414834
-            # 3	0.5555555555555556	0.7745966692414834
-        # n = 4
-            # ___________________________________________________
-            # i	weight - wi	abscissa - xi
-            # 1	0.6521451548625461	-0.3399810435848563
-            # 2	0.6521451548625461	0.3399810435848563
-            # 3	0.3478548451374538	-0.8611363115940526
-            # 4	0.3478548451374538	0.8611363115940526
-        # n = 5
-            # ___________________________________________________
-            # i	weight - wi	abscissa - xi
-            # 1	0.5688888888888889	0.0000000000000000
-            # 2	0.4786286704993665	-0.5384693101056831
-            # 3	0.4786286704993665	0.5384693101056831
-            # 4	0.2369268850561891	-0.9061798459386640
-            # 5	0.2369268850561891	0.9061798459386640
+        print('========================TESTING: 1D GAUSS QUADRATURE=======================')
+        dists2test = [cp.Uniform(-1,1), cp.Normal(), cp.Gamma(1,1), cp.Beta(1,1)]
+        rules2test = ['leg', 'hem', 'lag', 'jacobi']
+        order2test = [2,3,4,5,6,7,8]
+        for idist2test, irule2test in zip(dists2test, rules2test):
+            print('-'*50)
+            print('>>> Gauss Quadrature with polynominal: {}'.format(const.DOE_RULE_FULL_NAMES[irule2test.lower()]))
+            uqhelpers.blockPrint()
+            quad_doe = museuq.DoE('QUAD', irule2test, order2test, idist2test)
+            museuq_samples = quad_doe.get_samples()
+            # quad_doe.disp()
+            uqhelpers.enablePrint()
+            if irule2test == 'hem':
+                for i, iorder in enumerate(order2test):
+                    print('>>> order : {}'.format(iorder))
+                    coord1d_e, weight1d_e = np.polynomial.hermite_e.hermegauss(iorder)
+                    print('{:<15s}: {}'.format('probabilist', np.around(coord1d_e,2)))
+                    coord1d, weight1d = np.polynomial.hermite.hermgauss(iorder)
+                    print('{:<15s}: {}'.format('physicist', np.around(coord1d,2)))
+                    print('{:<15s}: {}'.format('museuq', np.around(np.squeeze(museuq_samples[i][:-1,:]),2)))
+
+            elif irule2test == 'leg':
+                for i, iorder in enumerate(order2test):
+                    print('>>> order : {}'.format(iorder))
+                    coord1d, weight1d = np.polynomial.legendre.leggauss(iorder)
+                    print('{:<15s}: {}'.format('numpy ', np.around(coord1d,2)))
+                    print('{:<15s}: {}'.format('museuq', np.around(np.squeeze(museuq_samples[i][:-1,:]),2)))
+            elif irule2test == 'lag':
+                for i, iorder in enumerate(order2test):
+                    print('>>> order : {}'.format(iorder))
+                    coord1d, weight1d = np.polynomial.laguerre.laggauss(iorder)
+                    print('{:<15s}: {}'.format('numpy ', np.around(coord1d,2)))
+                    print('{:<15s}: {}'.format('museuq', np.around(np.squeeze(museuq_samples[i][:-1,:]),2)))
+            elif irule2test == 'jacobi':
+                print('NOT TESTED YET')
 
 
-        # dist_zeta = cp.Iid(cp.Uniform(0,1),2) 
-
-        for dist, doe_rule in zip(quadrature_dist_to_test, quadrature_rule_to_test):
-            print('Gauss Quadrature with polynominal: {}'.format(const.DOE_RULE_FULL_NAMES[doe_rule.lower()]))
-            doe_method, doe_rule, doe_orders = 'QUAD', doe_rule, [2,3,4,5,6]
-            quad_doe = museuq.DoE(doe_method, doe_rule, doe_orders, dist)
-            samples_zeta= quad_doe.get_samples()
-            quad_doe.disp()
         print('Compared results here: https://keisan.casio.com/exec/system/1329114617')
 
     def test_gpce(self):
@@ -70,7 +69,7 @@ class BasicTestSuite(unittest.TestCase):
         dist_zeta0          = cp.Normal()
         for i, igpce_dist in enumerate(gpce_dist_to_test):
             dist_zeta1 = gpce_opt_dist[i]
-            print('>> Testing # {:d}: gpce: {}, zeta0: {} , zeta1: {}'.format(i, igpce_dist, dist_zeta0, dist_zeta1 ))
+            print('>>> Testing # {:d}: gpce: {}, zeta0: {} , zeta1: {}'.format(i, igpce_dist, dist_zeta0, dist_zeta1 ))
             for ipoly_order in npoly_orders:
                 print('  Polynomial order: {:d}'.format(ipoly_order))
                 ## gPCE with hermite chaos
@@ -78,10 +77,12 @@ class BasicTestSuite(unittest.TestCase):
                 quad_doe = museuq.DoE('QUAD', 'hem', [ipoly_order+1], dist_zeta0)
                 samples_zeta= quad_doe.get_samples()
                 zeta_cor, zeta_weight = samples_zeta[0]
+                zeta_cor = zeta_cor.reshape((len(dist_zeta0),-1))
                 x_cor = igpce_dist.inv(dist_zeta0.cdf(zeta_cor))
                 zeta_poly, zeta_norms = cp.orth_ttr(ipoly_order, dist_zeta0, retall=True)
-                x_hat,coeffs = cp.fit_quadrature(zeta_poly, zeta_cor, zeta_weight, np.squeeze(x_cor), retall=True)
+                x_hat,coeffs = cp.fit_quadrature(zeta_poly, zeta_cor, zeta_weight,np.squeeze(x_cor),retall=True)
                 uqhelpers.enablePrint()
+
                 print('\t Hermite: {}'.format( np.around(coeffs,4)))
 
                 ## gPCE with optimal chaos
@@ -89,6 +90,7 @@ class BasicTestSuite(unittest.TestCase):
                 quad_doe = museuq.DoE('QUAD', gpce_opt_rule[i], [ipoly_order+1], dist_zeta1)
                 samples_zeta= quad_doe.get_samples()
                 zeta_cor, zeta_weight = samples_zeta[0]
+                zeta_cor = zeta_cor.reshape((len(dist_zeta1),-1))
                 x_cor = igpce_dist.inv(dist_zeta1.cdf(zeta_cor))
                 zeta_poly, zeta_norms = cp.orth_ttr(ipoly_order, dist_zeta1, retall=True)
                 x_hat,coeffs = cp.fit_quadrature(zeta_poly, zeta_cor, zeta_weight, np.squeeze(x_cor), retall=True)
@@ -124,6 +126,36 @@ class BasicTestSuite(unittest.TestCase):
         tmax,dt =1000, 0.1
         t = np.arange(0,tmax, dt)
         y = museuq.solver.dynamic_models.linear_oscillator(t,x)
+        np.save('test_linear_oscillator_y',y)
+
+    def test_exceedance(self):
+        y = np.load('DoE_McRE3R0_y_stats.npy')
+        y = np.squeeze(y[:,4,:2]).T
+        print('y shape: {}'.format(y.shape))
+        y_excd=uqhelpers.get_exceedance_data(y, 1e-2)
+        np.save('test_linear_oscillator_excd', y_excd)
+
+
+    def test_Solver_run(self):
+        # data_set  = np.load('EC_pfe4.npy')
+        # x_samples = data_set.T
+        # print(x_samples.shape)
+
+        data_set  = np.load('DoE_McRE3R0.npy')
+        x_samples = data_set[2:,:]
+
+        model_name  = 'linear_oscillator'
+        solver      = museuq.Solver(model_name, x_samples)
+        kwargs      = {'doe_method': 'MCS'}
+        samples_y   = solver.run(**kwargs )
+        np.save('test_linear_oscillator_y', samples_y)
+        # filename_tags = ['R0']
+        # filename_tags = [itag+'_y' for itag in filename_tags]
+        # museuq_dataio.save_data(samples_y, 'test_linear_oscillator', os.getcwd(), filename_tags)
+        samples_y_stats = solver.get_stats()
+        np.save('test_linear_oscillator_y_stats', samples_y_stats)
+        # filename_tags = [itag+'_y_stats' for itag in filename_tags]
+        # museuq_dataio.save_data(samples_y_stats, 'test_linear_oscillator', os.getcwd(), filename_tags)
 
     def test_surrogate_model(self):
         print('========================TESTING: SurrogateModel.fit(), ~Normal =======================')
@@ -156,6 +188,24 @@ class BasicTestSuite(unittest.TestCase):
             print('Target: {}'.format(isolver_str))
             for i, ipoly_coeffs in enumerate(pce_model.poly_coeffs):
                 print('{:<6s}: {}'.format('museuq'*(i==0), np.around(ipoly_coeffs,4)))
+
+    def test_Kvitebjorn(self):
+        print('========================TESTING: Kvitebjorn =======================')
+
+
+        hs1     = np.linspace(0,2.9,291)
+        hs2     = np.linspace(2.90,20, 1711)
+        hs      = np.hstack((hs1, hs2))
+        hs_pdf  = Kvitebjorn.hs_pdf(hs) 
+        np.save(os.path.join(data_dir, 'Kvitebjorn_hs'), np.vstack((hs, hs_pdf)))
+
+
+        u = np.arange(0,1,1000)
+
+        n = 1e6 
+        samples_x = Kvitebjorn.samples(n)
+        np.save(os.path.join(data_dir, 'Kvitebjorn_samples_n'), samples_x)
+
 
 
         # print('========================TESTING: SurrogateModel.fit(), Generalized  ====================')
