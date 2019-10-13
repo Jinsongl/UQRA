139fac9ce0d0e97bc53629c3ad87fd0eca59ab2c
Jinsong <jinsongliu@utexas.edu>
2019-10-13 10:13:25 -0500

    Modifications on ObserveError class: 1. name change: ErrorType -> ObserveError 2. Add ObserveError.error() method to generate error samples 3. remove tolsit method 4. add __repr__, __str__ method and info()
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   utilities/classes.py

--- museuq/utilities/classes.py
+++ museuq/utilities/classes.py
@@ -12,68 +12,90 @@
 import numpy as np
 import sys, os
 from datetime import datetime
-class ErrorType():
-    def __init__(self, name=None, params=None, size=None):
+class ObserveError():
+    def __init__(self, name=None, **kwargs):
         """
         name:   string, error distribution name or None if not defined
-        params: list of error distribution parameters, float or array_like of floats 
+        """
+        self.name   = name if name else 'None' 
+        self.loc    = kwargs.get('loc', 0)
+        self.scale  = kwargs.get('scale', 1)
+        self.cov    = kwargs.get('cov', False)
+        # self.params = None 
+        # self.size   = None 
+
+    def error(self, size, *params):
+        """
+        Generate observe errors from specified error type 
+
+        Arguments:
+        params: args
+        list of error distribution parameters, float or array_like of floats 
                 [ [mu1, sigma1, ...]
                   [mu2, sigma2, ...]
                   ...]
-        size:   list of [int or tuple of ints], optional
+        size : int or tuple of ints, optional
             -- Output shape:
-            If the given shape is, e.g., (m, n, k), then m * n * k samples are drawn. 
+            If the given shape is, e.g., (m, n, k), then m * n * k observe errors. 
             If size is None (default), a single value is returned if loc and scale are both scalars. 
-            Otherwise, np.broadcast(loc, scale).size samples are drawn.
-        """
-        assert name is None or isinstance(name, str) 
-        if name is None:
-            self.name   = 'Free' 
-            self.params = None 
-            self.size   = None 
-        elif len(params) == 1:
-            self.name   = name
-            self.params = params[0]
-            self.size   = size[0] if size else None
-        else:
-            self.name   = [] 
-            for _ in range(len(params)):
-                self.name.append(name) 
-            self.size   = size if size else [None] * len(params)
-            self.params = params
-
-    def tolist(self, ndoe):
-        """
+            Otherwise, np.broadcast(loc, scale).size observe errors.
 
         """
-        if not isinstance(self.name, list):
-            return [ErrorType(self.name, [self.params], [self.size])] * ndoe
-        else:
-            assert len(self.name) == ndoe,"ErrorType.name provided ({:2d}) doesn't match number of DoE expected ({:2d})".format(len(self.name), ndoe)
-            error_type_list = []
-            for i in range(ndoe):
-                error_type_list.append(ErrorType(self.name[i], [self.params[i]], self.size[i]))
-            return error_type_list
+        loc, scale = params if self.cov else (self.loc, self.scale)
 
-    def disp(self):
-        if self.name.upper() == 'FREE':
-            print(r'   * Short-term/error distribution parameters: noise free')
-        else:
-            print(r'   * Short-term/error distribution parameters:')
-            print(r'     ∙ {:<15s} : {}'.format('dist_name', self.name))
-            for i, ierror_params in enumerate(self.params):
-                ierror_params_shape = []
-                for iparam in ierror_params:
-                    if np.isscalar(iparam):
-                        ierror_params_shape.append(1)
-                    else:
-                        ierror_params_shape.append(np.array(iparam).shape)
+        if self.name.upper() == 'NONE':
+            observe_errors = 0
 
-                if i == 0:
-                    print(r'     ∙ {:<15s} : {}'.format('dist_params',ierror_params_shape))
-                else:
-                    print(r'     ∙ {:<15s} : {}'.format('',ierror_params_shape))
-            print(r'     ∙ {:<15s} : {}'.format('dist_size', self.size))
+        elif self.name.upper() == 'NORMAL':
+            observe_errors = np.random.normal(loc=loc, scale=scale, size=size) 
+
+        elif self.name.upper() == 'GUMBEL':
+            observe_errors = np.random.gumbel(loc=loc, scale=scale, size=size)
+
+        # elif self.name.upper() == 'WEIBULL':
+            # observe_errors = scale * np.random.weibull(shape, size=size)
+        else:
+            raise NotImplementedError("{:s} error is not implemented".format())
+        return observe_errors
+    
+    # def tolist(self, ndoe):
+        # """
+
+        # """
+        # if not isinstance(self.name, list):
+            # return [ObserveError(self.name, [self.params], [self.size])] * ndoe
+        # else:
+            # assert len(self.name) == ndoe,"ObserveError.name provided ({:2d}) doesn't match number of DoE expected ({:2d})".format(len(self.name), ndoe)
+            # error_type_list = []
+            # for i in range(ndoe):
+                # error_type_list.append(ObserveError(self.name[i], [self.params[i]], self.size[i]))
+            # return error_type_list
+
+
+    def __repr__(self):
+        return "ObserveError({:s})".format(self.name)
+    def __str__(self):
+        return "ObserveError({:s})".format(self.name)
+
+    # def disp(self):
+        # if self.name.upper() == 'NONE':
+            # print(r'   * Short-term/error distribution parameters: None')
+        # else:
+            # print(r'   * Short-term/error distribution parameters:')
+            # print(r'     ∙ {:<15s} : {}'.format('dist_name', self.name))
+            # for i, ierror_params in enumerate(self.params):
+                # ierror_params_shape = []
+                # for iparam in ierror_params:
+                    # if np.isscalar(iparam):
+                        # ierror_params_shape.append(1)
+                    # else:
+                        # ierror_params_shape.append(np.array(iparam).shape)
+
+                # if i == 0:
+                    # print(r'     ∙ {:<15s} : {}'.format('dist_params',ierror_params_shape))
+                # else:
+                    # print(r'     ∙ {:<15s} : {}'.format('',ierror_params_shape))
+            # print(r'     ∙ {:<15s} : {}'.format('dist_size', self.size))
 
 class Logger(object):
     def __init__(self):
