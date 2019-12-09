97b71eb4f53f6120900b3ac088f7e729db705844
Jinsong <jinsongliu@utexas.edu>
2019-12-09 14:29:38 -0600

    Update museuq/__init__.py with new ExperimentDesign base class

--- museuq/__init__.py
+++ museuq/__init__.py
@@ -1,18 +1,22 @@
 """
-Uncertainty Quantifican Toolbox 
+Top-level package for Uncertainty Quantifican Toolbox 
 ===============================
-
-This module contains tools for performing uncertainty
-quantification 
+This module contains tools for performing uncertainty quantification 
 """
+
 # The values are copied by reference, 
 # So if you were to mutate MY_CONSTANT in any of the modules, it would mutate everywhere. 
 # If you were to re-assign MY_CONSTANT in any of the modules, it would only affect that module. In this case, you must reference by attribute, i.e. mypackage.constants.MY_CONSTANT
-# from museuq.constants import *
-#from museuq.setup import setup 
+
+import importlib
+import numpy as np
+import pandas as pd
+
 from museuq.doe.ExperimentDesign import ExperimentDesign as DoE
+from museuq.doe.quadrature import QuadratureDesign
+from museuq.doe.random_design import RandomDesign
+from museuq.doe.lhs import LatinHyperCube as LHS
 from museuq.solver.Solver import Solver
 from museuq.surrogate_model.SurrogateModel import SurrogateModel
 from museuq.simParameters import simParameters
-
 
