305613ccbfdcc2454705d5e7b1d8e40cd3ced376
Jinsong <jinsongliu@utexas.edu>
2019-12-09 14:31:37 -0600

    update museuq/doe/__init__.py with ExperimentaDesign base class

--- museuq/doe/__init__.py
+++ museuq/doe/__init__.py
@@ -16,23 +16,12 @@ Reference:
     SIAM Journal on Scientific Computing 38.1 (2016): A385-A411.
 ===============================
 """
+from museuq.doe.base import ExperimentalDesign
+from museuq.doe.quadrature import QuadratureDesign
+from museuq.doe.random_design import RandomDesign
 
-DOE_METHOD_FULL_NAMES = {
-    "GQ"    : "Quadrature"  , "QUAD"  : "Quadrature",
-    "MC"    : "Monte Carlo" , "FIX"   : "Fixed point"
-    } 
-
-DOE_RULE_FULL_NAMES = {
-    "CC": "clenshaw_curtis"  , "LEG"   : "gauss_legendre"  , "PAT"   : "gauss_patterson",
-    "GK": "genz_keister"     , "GWEL"   : "golub_welsch"    , "LEJA"   : "leja",
-    "HEM": "gauss_hermite"    ,"LAG"  : "gauss_laguerre"  , "CHEB": "gauss_chebyshev",
-    "HERMITE"   :"gauss_hermite",
-    "LEGENDRE"  :"gauss_legendre",
-    "JACOBI"    :"gauss_jacobi",
-    "R": "Pseudo-Random", "RG": "Regular Grid", "NG": "Nested Grid", "L": "Latin Hypercube",
-    "S": "Sobol", "H":"Halton", "M": "Hammersley",
-    "FIX": "Fixed point"
-    }
-# import doe_generator
-
+__all__= (
+        'ExperimentalDesign', 
+        'QuadratureDesign',
+        )
 
