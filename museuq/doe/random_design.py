5b862c061a05fa21594d51025e3ea9ed0202a2cd
Jinsong <jinsongliu@utexas.edu>
2019-12-20 16:21:08 -0600

    random_design: 1. modifications based on changes in base class 2. rnadom variable for specified distributions are specified in scipoy.stats.dist_name.rvs(n, paramsters) format
    Pay attention to the (loc, scale) parameters used in scipy
     On branch feat/newframe
     Your branch is up to date with 'origin/feat/newframe'.
    
     Changes to be committed:
    	modified:   random_design.py

--- museuq/doe/random_design.py
+++ museuq/doe/random_design.py
@@ -13,12 +13,13 @@ from museuq.doe.base import ExperimentalDesign
 from museuq.utilities.decorators import random_state
 from museuq.utilities.helpers import num2print
 import numpy as np
+import scipy
 import itertools
 
 class RandomDesign(ExperimentalDesign):
     """ Experimental Design with random sampling methods"""
 
-    def __init__(self, method, n, dist_names, params=None, ndim=1, *args, **kwargs):
+    def __init__(self, method, *args, **kwargs):
         """
         "Random"/Quasi random sampling design, dist_names are independent 
         Arguments:
@@ -32,49 +33,14 @@ class RandomDesign(ExperimentalDesign):
         params: list of params set for each dist, set is given in tuple
         """
         super().__init__(*args, **kwargs)
-        self.method     = method 
-        self.n_samples  = int(n)
-        self.ndim       = ndim
-        if self.ndim == 1:
-            if isinstance(dist_names, str):
-                self.dist_names = [dist_names, ]
-            elif isinstance(dist_names, list):
-                assert len(dist_names) ==1
-                self.dist_names = dist_names
-            else:
-                raise ValueError('Wrong dist_names format is provided')
-        else:
-            if isinstance(dist_names, str):
-                self.dist_names = [dist_names,] * self.ndim
-            elif isinstance(dist_names, list):
-                if len(dist_names) == self.ndim:
-                    self.dist_names = dist_names
-                elif len(dist_names) == 1:
-                    self.dist_names = dist_names * self.ndim
-                else:
-                    raise ValueError('Expecting {:d} random variables, but {:d} are given'.format(self.ndim, len(dist_names)))
-            else:
-                raise ValueError('Wrong dist_names format is provided')
-
-        if params is None:
-            self.dist_params = [None, ] * self.ndim
-        elif isinstance(params, list):
-            ### when only one set is given, assume to be for the first distribution assigned. Others are assumed to be None
-            for _ in range(len(params), self.ndim):
-                params.append(None) 
-            self.dist_params = params
-        elif isinstance(params, tuple):
-            self.dist_params = [None, ] * self.ndim
-            self.dist_params[0] = params
-
-        self.filename    = '_'.join(['DoE', self.method.capitalize() + num2print(self.n_samples)])
-
+        self.method   = method 
+        self.filename = '_'.join(['DoE', self.method.capitalize() + num2print(self.n_samples)])
 
     def __str__(self):
         message1 = '  > Random Design with method: {:s}\n'.format(self.method)
         message2 = '    {:<15s}: {:.0E}\n'.format('# samples', self.n_samples)
         message3 = '    {:<15s}: {}\n'.format('Distributions', self.dist_names)
-        message4 = '    {:<15s}: {}'.format('Parameters', self.dist_params)
+        message4 = '    {:<15s}: {}'.format('Parameters', self.dist_theta)
         message  = message1 + message2 + message3 + message4
         return message
 
@@ -86,12 +52,12 @@ class RandomDesign(ExperimentalDesign):
         """
         if self.method.upper() in ['R', 'MC', 'MCS']:
             x_samples = []
-            for idist_name, idist_params in itertools.zip_longest(self.dist_names, self.dist_params):
-                dist_random = getattr(np.random, idist_name)
-                if idist_params is None:
-                    x_samples.append(dist_random(size=self.n_samples))
+            for idist_name, idist_theta in itertools.zip_longest(self.dist_names, self.dist_theta):
+                idist = getattr(scipy.stats.distributions, idist_name)
+                if idist_theta is None:
+                    x_samples.append(idist.rvs(size=self.n_samples))
                 else:
-                    x_samples.append(dist_random(*idist_params, size=self.n_samples))
+                    x_samples.append(idist.rvs(*idist_theta, size=self.n_samples))
             self.x = np.array(x_samples) 
         elif self.method.upper() in ['HALTON', 'HAL', 'H']:
             raise NotImplementedError 
