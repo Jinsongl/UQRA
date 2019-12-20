a096940ad520571ea693bf497e020677190dc1a2
Jinsong <jinsongliu@utexas.edu>
2019-12-20 16:15:35 -0600

    lhs: add u attributes: LHS from (0,1) first x: tranform from u to x specified by distributions  On branch feat/newframe  Your branch is up to date with 'origin/feat/newframe'.
     Changes to be committed:
    	modified:   lhs.py

--- museuq/doe/lhs.py
+++ museuq/doe/lhs.py
@@ -15,6 +15,7 @@ import pyDOE2
 import itertools
 from museuq.utilities.decorators import random_state
 from museuq.doe.base import ExperimentalDesign
+from museuq.utilities.helpers import num2print
 
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning) 
@@ -22,12 +23,12 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 class LatinHyperCube(ExperimentalDesign):
     """ Experimental Design with Latin Hyper Cube"""
 
-    def __init__(self, d, n, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
-        d: dimension of random variables
+        ndim: dimension of random variables
 
         Optional: 
-            n: number of samples to generate at each dimension, default: d
+            n: number of samples to generate at each dimension, default: ndim
             criterion: a string that tells lhs how to sample the points
                - default: “maximin” or “m”: maximize the minimum distance between points, but place the point in a randomized location within its interval
                -  None, simply randomizes the points within the intervals
@@ -37,34 +38,25 @@ class LatinHyperCube(ExperimentalDesign):
                - “correlation” or “corr”: minimize the maximum correlation coefficient
         """
         super().__init__(*args, **kwargs)
-
-        self.d         = d
-        self.n_samples = int(n)
         self.criterion = kwargs.get('criterion', 'maximin')
         self.iterations= kwargs.get('iterations', 5)
 
     def __str__(self):
-        return('Sampling method: {:<15s}, num. samples: {:d} '.format('LHS', self.n_samples))
+        return('Sampling method: {:<15s}, num. samples: {:ndim} '.format('LHS', self.n_samples))
 
     @random_state
-    def samples(self, dist_names='uniform', dist_params=[[0,1]]):
-        # with warnings.catch_warnings():
-            # warnings.simplefilter('ignore', RuntimeWarning)
-            # lhs_u = pyDOE2.lhs(self.d, samples=self.n_samples, criterion=self.criterion, iterations=self.iterations)
-        lhs_u = pyDOE2.lhs(self.d, samples=self.n_samples, criterion=self.criterion, iterations=self.iterations)
-        lhs_u = lhs_u.reshape(self.d, self.n_samples)
-        # print(lhs_u)
-        dist_names = [dist_names, ] * self.d if isinstance(dist_names, str) else dist_names
-        assert len(dist_names) == self.d
-        lhs_x = []
-        for ilhd_u, idist_name, idist_params in itertools.zip_longest(lhs_u, dist_names, dist_params):
+    def samples(self):
+        lhs_u   = pyDOE2.lhs(self.ndim, samples=self.n_samples, criterion=self.criterion, iterations=self.iterations)
+        lhs_u   = lhs_u.reshape(self.ndim, self.n_samples)
+        self.u  = lhs_u
+        lhs_x   = []
+        for ilhd_u, idist_name, idist_theta in zip(lhs_u, self.dist_names, self.dist_theta):
             idist = getattr(scipy.stats.distributions, idist_name)
-            print(idist_name)
-            print(idist_params)
-            if idist_params is None:
+            if idist_theta is None:
                 lhs_x.append(idist.ppf(ilhd_u))
             else:
-                lhs_x.append(idist(*idist_params).ppf(ilhd_u))
+                # print(idist_theta)
+                lhs_x.append(idist.ppf(ilhd_u, *idist_theta))
         self.x = np.array(lhs_x)
 
 
