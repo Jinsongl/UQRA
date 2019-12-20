d68a24e7c9acdd6b1f142d9783546592e1aa9a69
Jinsong <jinsongliu@utexas.edu>
2019-12-20 16:18:39 -0600

    modifications based on changes in base class 1. removed ndim, n_samples etc 2. doe.w always in shape(n,) 3. doe.x always in shape (ndim, n_samples), in 1D , (1, n_samples)
     On branch feat/newframe
     Your branch is up to date with 'origin/feat/newframe'.
    
     Changes to be committed:
    	modified:   quadrature.py

--- museuq/doe/quadrature.py
+++ museuq/doe/quadrature.py
@@ -19,6 +19,9 @@ DOE_METHOD_FULL_NAMES = {
     "MC"    : "Monte Carlo" , "FIX"   : "Fixed point"
     } 
 
+ASKEY_WIENER = {
+        'normal': 'hem', 'uniform': 'leg'
+        }
 DOE_RULE_FULL_NAMES = {
     "CC": "clenshaw_curtis"  , "LEG"   : "gauss_legendre"  , "PAT"   : "gauss_patterson",
     "GK": "genz_keister"     , "GWEL"   : "golub_welsch"    , "LEJA"   : "leja",
@@ -31,52 +34,24 @@ DOE_RULE_FULL_NAMES = {
     "FIX": "Fixed point"
     }
 class QuadratureDesign(ExperimentalDesign):
-    """ Experimental Design with Quadrature forms """
+    """ Experimental Design with Quadrature basis_names """
 
-    def __init__(self, forms, p, ndim, dist_params=None, *args, **kwargs):
+    def __init__(self, p, *args, **kwargs):
         """
         Parameters:
-            forms: str or list of str, polynomial basis form. hem: Hermtie, leg: Legendre
-            p:      int, number of quadrature points in each dimension
-            ndim:   int, dimension of variable
-            dist_params: list: corresponding distribution parameters
+            basis_names: str or list of str, polynomial basis form. hem: Hermtie, leg: Legendre
+            p    : int, number of quadrature points in each dimension
         """
         super().__init__(*args, **kwargs)
-        self.ndim   = ndim
-        self.p      = p
-        if self.ndim ==1 :
-            assert isinstance(forms, str)
-            self.forms       = forms
-            self.dist_params = dist_params
-            self.filename    = '_'.join(['DoE_Quad', self.forms.capitalize() + num2print(self.p)])
-        else:
-            if isinstance(forms, list):
-                if len(forms) == 1:
-                    self.forms = forms * self.ndim
-                else:
-                    assert len(forms) == self.ndim
-            elif isinstance(forms, str):
-                self.forms = [forms,] * self.ndim
-            else:
-                raise ValueError('QuadratureDesign.forms takes either str or list of str, but {} is given'.format(type(forms)))
-
-            self.filename    = '_'.join(['DoE_Quad', self.forms[0].capitalize() + num2print(self.p)])
-
-            if dist_params is None:
-                self.dist_params = [None,] * self.ndim
-            elif isinstance(dist_params, list) and isinstance(dist_params[0], list):
-                assert len(dist_params) == self.ndim
-                self.dist_params = dist_params
-            elif isinstance(dist_params, list) and not isinstance(dist_params[0], list):
-                self.dist_params = [dist_params,] * self.ndim
-            else:
-                raise ValueError('Wrong format given for dist_params')
+        self.p          = p
+        self.basis_names= ['None' if idist_names is None else ASKEY_WIENER[idist_names] for idist_names in self.dist_names]
+        self.filename   = '_'.join(['DoE', 'Quad'+self.basis_names[0].capitalize() + num2print(self.p)])
 
         ## results
         self.w           = []  # Gaussian quadrature weights corresponding to each quadrature node 
 
     def __str__(self):
-        return('Gauss Quadrature: {}, p={:d}, ndim={:d} '.format(self.forms, self.p, self.ndim))
+        return('Gauss Quadrature: {}, p={:d}, ndim={:d} '.format(self.basis_names, self.p, self.ndim))
 
     def samples(self):
         """
@@ -85,52 +60,54 @@ class QuadratureDesign(ExperimentalDesign):
         """
         coords, weights = [], [] 
 
-        if self.ndim == 1:
-            u, w = self._gen_quad_1d(self.forms, self.p, self.dist_params) 
-            self.u = u.reshape(1, -1) 
-            self.w = np.squeeze(w)
-        else:
-            for ipoly_form, idist_params in zip(self.forms, self.dist_params):
-                iu, iw = self._gen_quad_1d(ipoly_form, self.p, idist_params) 
-                coords.append(iu)
-                weights.append(iw)
-            self.u = np.array(list(itertools.product(*coords))).T
-            self.w = np.prod(np.array(list(itertools.product(*weights))).T, axis=0)
+        # if self.ndim == 1:
+            # u, w = self._gen_quad_1d(self.basis_names[0], self.p, self.dist_theta) 
+            # self.u = u.reshape(1, -1) 
+            # self.w = np.squeeze(w)
+        # else:
+        for ibasis_names, idist_theta in zip(self.basis_names, self.dist_theta):
+            iu, iw = self._gen_quad_1d(ibasis_names, self.p, idist_theta) 
+            coords.append(iu)
+            weights.append(iw)
+        self.u = np.array(list(itertools.product(*coords))).T
+        self.u = self.u.reshape(self.ndim, -1)
+        self.w = np.prod(np.array(list(itertools.product(*weights))).T, axis=0)
+        self.w = np.squeeze(self.w)
 
 
-    def _gen_quad_1d(self, poly_type, p, dist_params=None):
-        if poly_type.lower() in ['hem', 'hermite']:
+    def _gen_quad_1d(self, basis_name, p, dist_theta=None):
+        if basis_name.lower() in ['hem', 'hermite']:
             ## probabilists , chaospy orth_ttr generate probabilists orthogonal polynomial
             x, w = np.polynomial.hermite_e.hermegauss(p) 
-            mu, sigma = dist_params if dist_params else (0,1)
+            mu, sigma = dist_theta if dist_theta else (0,1)
             x = mu + sigma * x
             w = sigma * w
             # coord   = np.array(list(itertools.product(*[coord1d]*len(domain)))).T
             # weights = np.array(list(itertools.product(*[weight1d]*len(domain)))).T
             # weights = np.prod(weights, axis=0)
-        elif poly_type.lower() in ['leg', 'legendre']:
+        elif basis_name.lower() in ['leg', 'legendre']:
             x, w = np.polynomial.legendre.leggauss(p)
-            a, b = dist_params if dist_params else (-1,1)
+            a, b = (dist_theta[0], dist_theta[0] + dist_theta[1]) if dist_theta else (-1,1)
             x = (b-a)/2 * x + (a+b)/2
             w = (b-a)/2 * w
 
-        # elif poly_type in ['jac', 'jacobi']:
+        # elif basis_name in ['jac', 'jacobi']:
             # coord1d, weight1d = sp.special.roots_jacobi(order,4,2) #  np.polynomial.laguerre.laggauss(order)
             # coord   = np.array(list(itertools.product(*[coord1d]*len(domain)))).T
             # weights = np.array(list(itertools.product(*[weight1d]*len(domain)))).T
             # weights = np.prod(weights, axis=0)
 
-        # elif poly_type in ['lag', 'laguerre']:
+        # elif basis_name in ['lag', 'laguerre']:
             # coord1d, weight1d = np.polynomial.laguerre.laggauss(order)
             # coord   = np.array(list(itertools.product(*[coord1d]*len(domain)))).T
             # weights = np.array(list(itertools.product(*[weight1d]*len(domain)))).T
             # weights = np.prod(weights, axis=0)
 
-        # elif poly_type in ['cheb', 'chebyshev']:
+        # elif basis_name in ['cheb', 'chebyshev']:
             # coord1d, weight1d = np.polynomial.chebyshev.chebgauss(order)
             # coord   = np.array(list(itertools.product(*[coord1d]*len(domain)))).T
             # weights = np.array(list(itertools.product(*[weight1d]*len(domain)))).T
             # weights = np.prod(weights, axis=0)
         else:
-            raise NotImplementedError("Quadrature forms '{:s}' is not defined".format(forms))
+            raise NotImplementedError("Quadrature basis_names '{:s}' is not defined".format(basis_names))
         return x, w
