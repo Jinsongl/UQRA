c4717c426293e2722304a013c014f860de9b34ee
Jinsong <jinsongliu@utexas.edu>
2020-01-03 11:45:26 -0600

    Redefined moment and mquantiles: now input array could by multi-columns. multioutput parameter takes: 	'uniform_average' 	'raw_values'  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   metrics_collections.py

--- museuq/utilities/metrics_collections.py
+++ museuq/utilities/metrics_collections.py
@@ -29,15 +29,95 @@ from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_
 # other	–	                        sum(abs(x)**ord)**(1./ord)
 
 
-from scipy.stats import moment
-from scipy.stats.mstats import mquantiles
+def r2_score_adj(y_true, y_pred, num_predictor, sample_weight=None, multioutput='uniform_average'):
+    """
+    calculation of adjusted r2
+    Parameters
+        y_truearray-like of shape (n_samples,) or (n_samples, n_outputs)
+        Ground truth (correct) target values.
 
-def moments(y_true, y_pred, m=1):
-    res = [sp.stats.moment(y_true,moment=m), sp.stats.moment(y_pred,moment=m)]
+        y_predarray-like of shape (n_samples,) or (n_samples, n_outputs)
+        Estimated target values.
+
+        sample_weightarray-like of shape (n_samples,), optional
+        Sample weights.
+
+        num_predictor: number of independent variables
+
+        multioutputstring in [‘raw_values’, ‘uniform_average’, ‘variance_weighted’] or array-like of shape (n_outputs)
+        Defines aggregating of multiple output scores. Array-like value defines weights used to average scores.
+
+            ‘raw_values’ :
+            Returns a full set of scores in case of multioutput input.
+
+            ‘uniform_average’ :
+            Scores of all outputs are averaged with uniform weight.
+
+            ‘variance_weighted’ :
+            Scores of all outputs are averaged, weighted by the variances of each individual output.
+
+    Returns
+        scorefloat or ndarray of floats
+        The explained variance or ndarray if ‘multioutput’ is ‘raw_values’.
+
+
+    """
+    r2 = r2_score(y_true, y_pred, sample_weight=sample_weight, multioutput=multioutput)
+    n  = len(y_true) if y_true.ndim ==1 else y_true.shape[0]
+    r2_adj = 1 - (1-r2**2) * (n-1)/(n-num_predictor-1)
+    return r2_adj
+
+
+def mquantiles(a, prob=[0.25, 0.5, 0.75],axis=0,limit=(),multioutput='uniform_average'):
+    """
+    Parameters
+        a: array_like
+        Input data, as a sequence or array of dimension at most 2.
+
+        prob: array_like, optional
+        List of quantiles to compute.
+
+        axis: int, optional
+        Axis along which to perform the trimming. If None, the input array is first flattened. axis=0 default
+
+        limit: tuple, optional
+        Tuple of (lower, upper) values. Values of a outside this open interval are ignored.
+
+    """
+    ### output format (nprob, noutputs)
+    res =  sp.stats.mstats.mquantiles(a, prob=prob,axis=axis,limit=limit)
+
+    if multioutput.lower() == 'uniform_average':
+        if res.ndim == 1:
+            res = res 
+        elif res.ndim == 2:
+            res = np.mean(res,axis=1, dtype=np.float64)
+        else:
+            raise ValueError('Input data, as a sequence or array of dimension at most 2.')
+        # print('{:s} :{}'.format('mquantiles', res.shape))
+    elif multioutput.lower() == 'raw_values':
+        res = res
+    else:
+        raise NotImplementedError
     return res
 
-def upper_tails(y_true, y_pred, prob=[0.75, 0.9, 0.99], alphap=0.4, betap=0.4, axis=None, limit=()):
-    res = [sp.stats.mstats.mquantiles(y_true,prob=prob,alphap=alphap,betap=betap,axis=axis,limit=limit),
-           sp.stats.mstats.mquantiles(y_pred,prob=prob,alphap=alphap,betap=betap,axis=axis,limit=limit)]
+def moment(a, moment=1, axis=0, nan_policy='propagate',multioutput='uniform_average'):
+    ### output format (nmoments, noutputs)
+    res = sp.stats.moment(a, moment=moment, axis=axis, nan_policy=nan_policy)
+    if multioutput.lower() == 'uniform_average':
+        if res.ndim == 1:
+            res = res 
+        elif res.ndim == 2:
+            res = np.mean(res,axis=1, dtype=np.float64)
+        else:
+            raise ValueError('Input data, as a sequence or array of dimension at most 2.')
+        # print('{:s} :{}'.format('moments', res.shape))
+    elif multioutput.lower() == 'raw_values':
+        res = res
+    else:
+        raise NotImplementedError
     return res
+
+
+
 
