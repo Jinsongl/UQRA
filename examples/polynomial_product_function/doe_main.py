f7097ad0e455b45442231255b6e7c2e9a39a9b66
Jinsong <jinsongliu@utexas.edu>
2020-01-04 15:03:03 -0600

    update doe_main for polynomial product function

--- examples/polynomial_product_function/doe_main.py
+++ examples/polynomial_product_function/doe_main.py
@@ -13,14 +13,7 @@ import context
 import museuq
 import numpy as np, chaospy as cp, os, sys
 import warnings
-from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
-                                              ExpSineSquared, DotProduct,
-                                              ConstantKernel,WhiteKernel)
 from tqdm import tqdm
-from museuq.utilities import helpers as uqhelpers
-from museuq.utilities import metrics_collections as museuq_metrics
-from museuq.utilities import dataIO 
-from museuq.environment import Kvitebjorn
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 sys.stdout  = museuq.utilities.classes.Logger()
 
@@ -40,16 +33,7 @@ def main():
     # simparams.set_error()  # no observation error for sdof
     simparams.info()
 
-
-    # #### --------------------------------------------------------------------- ###
-    # #### ----------------- Define surrogate model parameters ----------------- ###
-    # #### --------------------------------------------------------------------- ###
-    # metamodel_params= {'cal_coeffs': 'Galerkin', 'dist_zeta': dist_zeta}
-    # metamodel_class = 'PCE'
-    # pce_fit_method  = 'OLS'
     solver = museuq.polynomial_product_function()
-    # museuq.Solver()
-    ### ------------------------------ Run DOE ----------------------------- ###
 
     ### 1. Random Design
     for r in tqdm(range(10)):
@@ -57,17 +41,17 @@ def main():
         doe.samples()
         doe.x = doe.u
         solver.run(doe.x)
-        doe_data = np.concatenate((doe.u, doe.x, solver.y.reshape(1,-1)), axis=0)
+        data = np.concatenate((doe.u, doe.x, solver.y.reshape(1,-1)), axis=0)
         filename = os.path.join(simparams.data_dir, doe.filename+'R{:d}'.format(r))
-        np.save(filename, doe_data)
+        np.save(filename, data)
 
     # # ### 2. Latin HyperCube Design
     # # for r in range(10):
         # # doe = museuq.LHS(n_samples=1e3,dist_names=['normal']*2,ndim=2,dist_theta=[(0,1)]*2)
         # # doe.samples()
-        # # doe_data = np.concatenate((doe.u, doe.x), axis=0)
+        # # data = np.concatenate((doe.u, doe.x), axis=0)
         # # filename = os.path.join(simparams.data_dir, doe.filename+'R{:d}'.format(r))
-        # # np.save(filename, doe_data)
+        # # np.save(filename, data)
 
 
     ### 3. Quadrature Design
@@ -78,54 +62,54 @@ def main():
         # print(' > {:<15s} : {}'.format('Experimental Design', doe))
         doe.x = doe.u 
         solver.run(doe.x)
-        doe_data = np.concatenate((doe.u, doe.x, doe.w.reshape(1,-1), solver.y.reshape(1,-1)), axis=0)
+        data = np.concatenate((doe.u, doe.x, doe.w.reshape(1,-1), solver.y.reshape(1,-1)), axis=0)
         filename = os.path.join(simparams.data_dir, doe.filename)
-        np.save(filename, doe_data)
+        np.save(filename, data)
 
 
 
-    ### 4. Optimal Design
-    # # data_dir = '/Users/jinsongliu/External/MUSE_UQ_DATA/linear_oscillator/Data'
-    # # data_dir = 'E:\Run_MUSEUQ'
-    # data_dir = os.getcwd()
-    # np.random.seed(100)
-    # # dist_x = cp.Normal()
+    ## 4. Optimal Design
+    # data_dir = '/Users/jinsongliu/External/MUSE_UQ_DATA/linear_oscillator/Data'
+    # data_dir = 'E:\Run_MUSEUQ'
+    data_dir = os.getcwd()
+    np.random.seed(100)
+    # dist_x = cp.Normal()
 
-    # ### 2D
-    # quad_orders = range(3,20)
-    # alpha = [1.0, 1.1, 1.3, 1.5, 2.0,2.5, 3.0,3.5, 5]
-    # dist_u= cp.Iid(cp.Normal(),2)
-    # for iquad_orders in quad_orders:
-        # basis = cp.orth_ttr(iquad_orders-1,dist_u)
-        # for r in range(4):
+    ### 2D
+    quad_orders = range(3,20)
+    alpha = [1.0, 1.1, 1.3, 1.5, 2.0,2.5, 3.0,3.5, 5]
+    dist_u= cp.Iid(cp.Normal(),2)
+    for iquad_orders in quad_orders:
+        basis = cp.orth_ttr(iquad_orders-1,dist_u)
+        for r in range(4):
             
-            # filename  = 'DoE_McsE6R{:d}.npy'.format(r)
-            # data_set  = np.load(os.path.join(data_dir, filename))
-            # samples_u = data_set[0:2, :]
-            # samples_x = data_set[2:4, :]
-            # samples_y = data_set[4,:].reshape(1,-1)
-            # print('Quadrature Order: {:d}'.format(iquad_orders))
-            # print('Candidate samples filename: {:s}'.format(filename))
-            # print('   >> Candidate sample set shape: {}'.format(samples_u.shape))
-            # design_matrix = basis(*samples_u).T
-            # print('   >> Candidate Design matrix shape: {}'.format(design_matrix.shape))
-            # for ia in alpha:
-                # print('   >> Oversampling rate : {:.2f}'.format(ia))
-                # doe_size = min(int(len(basis)*ia), 10000)
-                # doe = museuq.OptimalDesign('S', n_samples = doe_size )
-                # doe.samples(design_matrix, u=samples_u, is_orth=True)
-                # data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
-                # filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptS{:d}'.format(r,iquad_orders,doe_size))
-                # np.save(filename, data)
+            filename  = 'DoE_McsE6R{:d}.npy'.format(r)
+            data_set  = np.load(os.path.join(data_dir, filename))
+            samples_u = data_set[0:2, :]
+            samples_x = data_set[2:4, :]
+            samples_y = data_set[4,:].reshape(1,-1)
+            print('Quadrature Order: {:d}'.format(iquad_orders))
+            print('Candidate samples filename: {:s}'.format(filename))
+            print('   >> Candidate sample set shape: {}'.format(samples_u.shape))
+            design_matrix = basis(*samples_u).T
+            print('   >> Candidate Design matrix shape: {}'.format(design_matrix.shape))
+            for ia in alpha:
+                print('   >> Oversampling rate : {:.2f}'.format(ia))
+                doe_size = min(int(len(basis)*ia), 10000)
+                doe = museuq.OptimalDesign('S', n_samples = doe_size )
+                doe.samples(design_matrix, u=samples_u, is_orth=True)
+                data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
+                filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptS{:d}'.format(r,iquad_orders,doe_size))
+                np.save(filename, data)
 
-            # for ia in alpha:
-                # print('   >> Oversampling rate : {:.2f}'.format(ia))
-                # doe_size = min(int(len(basis)*ia), 10000)
-                # doe = museuq.OptimalDesign('D', n_samples = doe_size )
-                # doe.samples(design_matrix, u=samples_u, is_orth=True)
-                # data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
-                # filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptD{:d}'.format(r,iquad_orders,doe_size))
-                # np.save(filename, data)
+            for ia in alpha:
+                print('   >> Oversampling rate : {:.2f}'.format(ia))
+                doe_size = min(int(len(basis)*ia), 10000)
+                doe = museuq.OptimalDesign('D', n_samples = doe_size )
+                doe.samples(design_matrix, u=samples_u, is_orth=True)
+                data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
+                filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptD{:d}'.format(r,iquad_orders,doe_size))
+                np.save(filename, data)
                 
 
 
