9b2963fb763f6ddc18f41a7afb5bede9649b8ec8
Jinsong <jinsongliu@utexas.edu>
2020-01-04 17:06:52 -0600

    SurrogateModel.cal_scores: moments and mquantiles return vstacked ndarray. Easier to read data

--- museuq/surrogate_model/base.py
+++ museuq/surrogate_model/base.py
@@ -123,7 +123,9 @@ class SurrogateModel(object):
                 ### output format (nprob, noutput)
                 imetrics_value_true = imetric2call(y_true, prob=self.mquantiles_probs, axis=axis, multioutput=multioutput)
                 imetrics_value_pred = imetric2call(y_pred, prob=self.mquantiles_probs, axis=axis, multioutput=multioutput)
-                metrics_value.append([imetrics_value_true,imetrics_value_pred])
+                imetrics_value = np.vstack((imetrics_value_true,imetrics_value_pred))
+                print(imetrics_value.shape)
+                metrics_value.append(imetrics_value)
 
             elif imetrics.lower() == 'moment': 
                 ### output format (nmoment, noutput)
@@ -146,7 +148,9 @@ class SurrogateModel(object):
 
                 imetrics_value_true[mean_idx] = mean_true
                 imetrics_value_pred[mean_idx] = mean_pred
-                metrics_value.append([imetrics_value_true,imetrics_value_pred])
+                imetrics_value = np.vstack((imetrics_value_true,imetrics_value_pred))
+                print(imetrics_value.shape)
+                metrics_value.append(imetrics_value)
 
             elif imetrics.lower() == 'r2_score_adj':
                 ### output format (1,) or (noutput,)
