dbef5ac20f93856a630c15494f18ead06c5aa7a7
Jinsong <jinsongliu@utexas.edu>
2020-01-05 13:29:40 -0600

    doe pce file for four branch system

--- examples/four_branch_system/doe_main.py
+++ examples/four_branch_system/doe_main.py
@@ -13,14 +13,7 @@ import context
 import museuq
 import numpy as np, chaospy as cp, os, sys
 import warnings
-from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
-                                              ExpSineSquared, DotProduct,
-                                              ConstantKernel,WhiteKernel)
 from tqdm import tqdm
-from museuq.utilities import helpers as uqhelpers
-from museuq.utilities import metrics_collections as museuq_metrics
-from museuq.utilities import dataIO 
-from museuq.environment import Kvitebjorn
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 sys.stdout  = museuq.utilities.classes.Logger()
 
@@ -29,101 +22,91 @@ sys.stdout  = museuq.utilities.classes.Logger()
 
 def main():
     ## ------------------------ Parameters set-up ----------------------- ###
+    ndim        = 2
+    n_samples   = 1E6
     model_name  = 'four_branch_system'
     ## 1. Choose Wiener-Askey scheme random variable
-    dist_x      = cp.Iid(cp.Normal(),2) 
-    dist_zeta   = cp.Iid(cp.Normal(),2) 
+    dist_x      = cp.Iid(cp.Normal(),ndim) 
+    dist_zeta   = cp.Iid(cp.Normal(),ndim) 
     ## 2. If transformation needed, like Rosenblatt, need to be done here
     ## Perform Rosenblatt etc
     simparams = museuq.simParameters(model_name, dist_zeta)
     # simparams.set_error()  # no observation error for sdof
     simparams.info()
 
+    solver = museuq.four_branch_system()
 
-    # #### --------------------------------------------------------------------- ###
-    # #### ----------------- Define surrogate model parameters ----------------- ###
-    # #### --------------------------------------------------------------------- ###
-    # metamodel_params= {'cal_coeffs': 'Galerkin', 'dist_zeta': dist_zeta}
-    metamodel_class = 'PCE'
-    pce_fit_method  = 'OLS'
-    solver = museuq.Solver(model_name)
-    ### ------------------------------ Run DOE ----------------------------- ###
+    ### 1. Random Design
+    for r in tqdm(range(10)):
+        doe = museuq.RandomDesign('MCS', n_samples=n_samples, ndim=ndim, dist_names= ['normal',]*ndim, dist_theta=[(0,1),]*ndim)
+        doe.samples()
+        doe.x = doe.u
+        solver.run(doe.x)
+        data = np.concatenate((doe.u, doe.x, solver.y.reshape(1,-1)), axis=0)
+        filename = os.path.join(simparams.data_dir, doe.filename+'R{:d}'.format(r))
+        np.save(filename, data)
 
-    # ### 1. Random Design
-    # for r in range(10):
-        # doe = museuq.RandomDesign('MCS', n_samples=1e6, ndim=2, dist_names= ['normal',]*2, dist_theta=[(0,1),]*2)
+    # ### 2. Latin HyperCube Design
+    # for r in tqdm(range(10)):
+        # doe = museuq.LHS(n_samples=1e3,dist_names=['normal']*ndim,ndim=ndim,dist_theta=[(0,1)]*ndim)
         # doe.samples()
-        # doe.x = doe.u
-        # solver.run(doe.x)
-        # doe_data = np.concatenate((doe.u, doe.x, solver.y.reshape(1,-1)), axis=0)
+        # data = np.concatenate((doe.u, doe.x), axis=0)
         # filename = os.path.join(simparams.data_dir, doe.filename+'R{:d}'.format(r))
-        # np.save(filename, doe_data)
-
-    # # ### 2. Latin HyperCube Design
-    # # for r in range(10):
-        # # doe = museuq.LHS(n_samples=1e3,dist_names=['normal']*2,ndim=2,dist_theta=[(0,1)]*2)
-        # # doe.samples()
-        # # doe_data = np.concatenate((doe.u, doe.x), axis=0)
-        # # filename = os.path.join(simparams.data_dir, doe.filename+'R{:d}'.format(r))
-        # # np.save(filename, doe_data)
+        # np.save(filename, data)
 
 
-    # ### 3. Quadrature Design
-    # quad_orders = range(3,20)
-    # for iquad_orders in quad_orders:
-        # doe = museuq.QuadratureDesign(iquad_orders, ndim = 2, dist_names=['normal', 'normal'])
-        # doe.samples()
-        # print(' > {:<15s} : {}'.format('Experimental Design', doe))
-        # doe.x = doe.u 
-        # solver.run(doe.x)
-        # doe_data = np.concatenate((doe.u, doe.x, doe.w.reshape(1,-1), solver.y.reshape(1,-1)), axis=0)
-        # filename = os.path.join(simparams.data_dir, doe.filename)
-        # np.save(filename, doe_data)
+    ### 3. Quadrature Design
+    quad_orders = range(1,10)
+    for iquad_orders in tqdm(quad_orders):
+        doe = museuq.QuadratureDesign(iquad_orders, ndim = ndim, dist_names=['normal', ] *ndim)
+        doe.samples()
+        doe.x = doe.u 
+        solver.run(doe.x)
+        data = np.concatenate((doe.u, doe.x, doe.w.reshape(1,-1), solver.y.reshape(1,-1)), axis=0)
+        filename = os.path.join(simparams.data_dir, doe.filename)
+        np.save(filename, data)
 
 
+    # ### 4. Optimal Design
+    # # data_dir = '/Users/jinsongliu/External/MUSE_UQ_DATA/linear_oscillator/Data'
+    # # data_dir = 'E:\Run_MUSEUQ'
+    # data_dir ='E:\polynomial_square_root'
+    # np.random.seed(100)
 
-    ### 4. Optimal Design
-    # data_dir = '/Users/jinsongliu/External/MUSE_UQ_DATA/linear_oscillator/Data'
-    # data_dir = 'E:\Run_MUSEUQ'
-    data_dir = os.getcwd()
-    np.random.seed(100)
-    # dist_x = cp.Normal()
-
-    ### 2D
-    quad_orders = range(3,20)
-    alpha = [1.0, 1.1, 1.3, 1.5, 2.0,2.5, 3.0,3.5, 5]
-    dist_u= cp.Iid(cp.Normal(),2)
-    for iquad_orders in quad_orders:
-        basis = cp.orth_ttr(iquad_orders-1,dist_u)
-        for r in range(4):
+    # quad_orders = range(1,10)
+    # alpha       = [1.0, 1.1, 1.3, 1.5, 2.0,2.5, 3.0,3.5, 5]
+    # for iquad_orders in tqdm(quad_orders):
+        # basis = cp.orth_ttr(iquad_orders-1,dist_zeta)
+        # for r in range(4):
             
-            filename  = 'DoE_McsE6R{:d}.npy'.format(r)
-            data_set  = np.load(os.path.join(data_dir, filename))
-            samples_u = data_set[0:2, :]
-            samples_x = data_set[2:4, :]
-            samples_y = data_set[4,:].reshape(1,-1)
-            print('Quadrature Order: {:d}'.format(iquad_orders))
-            print('Candidate samples filename: {:s}'.format(filename))
-            print('   >> Candidate sample set shape: {}'.format(samples_u.shape))
-            design_matrix = basis(*samples_u).T
-            print('   >> Candidate Design matrix shape: {}'.format(design_matrix.shape))
-            for ia in alpha:
-                print('   >> Oversampling rate : {:.2f}'.format(ia))
-                doe_size = min(int(len(basis)*ia), 10000)
-                doe = museuq.OptimalDesign('S', n_samples = doe_size )
-                doe.samples(design_matrix, u=samples_u, is_orth=True)
-                data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
-                filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptS{:d}'.format(r,iquad_orders,doe_size))
-                np.save(filename, data)
+            # filename  = 'DoE_McsE6R{:d}.npy'.format(r)
+            # data_set  = np.load(os.path.join(data_dir, filename))
+            # samples_u = data_set[0:2, :]
+            # samples_x = data_set[2:4, :]
+            # samples_y = data_set[5  , :].reshape(1,-1)
+            # print('Quadrature Order: {:d}'.format(iquad_orders))
+            # print('Candidate samples filename: {:s}'.format(filename))
+            # print('   >> Candidate sample set shape: {}'.format(samples_u.shape))
+            # design_matrix = basis(*samples_u).T
+            # print('   >> Candidate Design matrix shape: {}'.format(design_matrix.shape))
+            
+            # # for ia in alpha:
+            # #     print('   >> Oversampling rate : {:.2f}'.format(ia))
+            # #     doe_size = min(int(len(basis)*ia), int(n_samples))
+            # #     doe = museuq.OptimalDesign('S', n_samples = doe_size )
+            # #     doe.samples(design_matrix, u=samples_u, is_orth=True)
+            # #     data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
+            # #     filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptS{:d}'.format(r,iquad_orders,doe_size))
+            # #     np.save(filename, data)
 
-            for ia in alpha:
-                print('   >> Oversampling rate : {:.2f}'.format(ia))
-                doe_size = min(int(len(basis)*ia), 10000)
-                doe = museuq.OptimalDesign('D', n_samples = doe_size )
-                doe.samples(design_matrix, u=samples_u, is_orth=True)
-                data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
-                filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptD{:d}'.format(r,iquad_orders,doe_size))
-                np.save(filename, data)
+            # for ia in alpha:
+                # print('   >> Oversampling rate : {:.2f}'.format(ia))
+                # doe_size = min(int(len(basis)*ia), int(n_samples))
+                # doe = museuq.OptimalDesign('D', n_samples = doe_size )
+                # doe.samples(design_matrix, u=samples_u, is_orth=True)
+                # data = np.concatenate((doe.I.reshape(1,-1),doe.u,samples_x[:,doe.I], samples_y[:,doe.I]), axis=0)
+                # filename = os.path.join(data_dir, 'DoE_McsE6R{:d}_q{:d}_OptD{:d}'.format(r,iquad_orders,doe_size))
+                # np.save(filename, data)
                 
 
 
--- examples/four_branch_system/pce_main.py
+++ examples/four_branch_system/pce_main.py
@@ -13,80 +13,68 @@ import context
 import museuq
 import numpy as np, chaospy as cp, os, sys
 import warnings
-from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
-                                              ExpSineSquared, DotProduct,
-                                              ConstantKernel,WhiteKernel)
 from tqdm import tqdm
 from museuq.utilities import helpers as uqhelpers
-from museuq.utilities import metrics_collections as museuq_metrics
-from museuq.utilities import dataIO 
-from museuq.environment import Kvitebjorn
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 sys.stdout  = museuq.utilities.classes.Logger()
 
 def main():
     ## ------------------------ Parameters set-up ----------------------- ###
-    model_name  = 'polynomial_square_root_function'
+    ndim        = 2
+    model_name  = 'four_branch_system'
     ## 1. Choose Wiener-Askey scheme random variable
-    dist_x      = cp.Iid(cp.Normal(),2) 
-    dist_zeta   = cp.Iid(cp.Normal(),2) 
+    dist_x      = cp.Iid(cp.Normal(),ndim) 
+    dist_zeta   = cp.Iid(cp.Normal(),ndim) 
     ## 2. If transformation needed, like Rosenblatt, need to be done here
     ## Perform Rosenblatt etc
     simparams = museuq.simParameters(model_name, dist_zeta)
     # simparams.set_error()  # no observation error for sdof
     simparams.info()
 
-
-
     #### ----------------------- Build PCE Surrogate Model -------------------- ###
     metamodel_class = 'PCE'
-    quad_orders     = range(3,8)
-    upper_tail_probs= [0.99,0.999]
+    quad_orders     = range(3,20)
+    upper_tail_probs= [0.999, 0.9999,0.99999]
     moment2cal      = [1,2,3,4]
     metrics2cal     = [ 'explained_variance_score', 'mean_absolute_error', 'mean_squared_error',
-                'median_absolute_error', 'r2_score', 'moment', 'mquantiles']
+                'median_absolute_error', 'r2_score', 'r2_score_adj', 'moment', 'mquantiles']
 
-    ### 1. PCE model based on quadrature design points, fitting with GLK and OLS
+    # ### 1. PCE model based on quadrature design points, fitting with GLK and OLS
 
-    # pce_fit_method  = 'OLS'
-    # for iquad_orders in quad_orders:
-        # poly_order = iquad_orders - 1
-        # ### ============ Get training points ============
-        # filename = 'DoE_QuadHem{:d}.npy'.format(iquad_orders)
-        # data_set = np.load(os.path.join(simparams.data_dir, filename))
-        # print('  > {:<10s}: {:s}'.format('filename', filename))
-        # train_u  = data_set[0:2,:] 
-        # train_x  = data_set[2:4,:] 
-        # train_w  = data_set[4,:]
-        # train_y  = data_set[5,:]
+    pce_fit_method  = 'GLK'
+    for iquad_orders in quad_orders:
+        poly_order = iquad_orders - 1
+        ### ============ Get training points ============
+        filename = 'DoE_QuadHem{:d}.npy'.format(iquad_orders)
+        data_set = np.load(os.path.join(simparams.data_dir, filename))
+        print('  > {:<10s}: {:s}'.format('filename', filename))
+        train_u  = data_set[0:2,:] 
+        train_x  = data_set[2:4,:] 
+        train_w  = data_set[4,:]
+        train_y  = data_set[5,:]
 
-        # ### ============ Get Surrogate Model for each QoI============
-        # pce_model = museuq.PCE(poly_order, dist_zeta)
-        # # print(len(pce_model.basis[0]))
-        # # pce_model.fit(train_u, train_y, w=train_w, fit_method=pce_fit_method)
-        # pce_model.fit(train_u, train_y, fit_method=pce_fit_method)
-        # # print(pce_model.poly_coeffs)
-        # # print(pce_model.basis_coeffs)
+        ### ============ Get Surrogate Model for each QoI============
+        pce_model = museuq.PCE(poly_order, dist_zeta)
+        pce_model.fit(train_u, train_y, w=train_w, fit_method=pce_fit_method)
 
-        # ### ============ Validating surrogate models at training points ============
+        ### ============ Validating surrogate models at training points ============
+        for r in range(10):
+            filename = 'DoE_McsE6R{:d}.npy'.format(r)
+            data_set = np.load(os.path.join(simparams.data_dir, filename))
+            valid_u  = data_set[0:2,:]
+            valid_x  = data_set[2:4,:]
+            valid_y  = data_set[4  ,:]
 
-        # for r in range(10):
-            # filename = 'DoE_McsE6R{:d}.npy'.format(r)
-            # data_set = np.load(os.path.join(simparams.data_dir, filename))
-            # valid_u  = data_set[0:2,:]
-            # valid_x  = data_set[2:4,:]
-            # valid_y  = data_set[4  ,:]
+            pce_model.predict(valid_u, valid_y, metrics=metrics2cal, prob=upper_tail_probs, moment=moment2cal)
+            filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_y.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
+            np.save(os.path.join(simparams.data_dir, filename), pce_model.y_pred[0])
 
-            # pce_valid_y, pce_valid_score = pce_model.predict(valid_u, valid_y, metrics=metrics2cal, prob=upper_tail_probs, moment=moment2cal)
-            # filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_y.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
-            # np.save(os.path.join(simparams.data_dir, filename), pce_valid_y)
+            filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_score.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
+            np.save(os.path.join(simparams.data_dir, filename), pce_model.scores[0])
 
-            # pce_valid_y_ecdf = uqhelpers.get_exceedance_data(pce_valid_y, 1e-5)
-            # filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_y_ecdf.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
-            # np.save(os.path.join(simparams.data_dir, filename), pce_valid_y_ecdf)
-
-            # filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_score.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
-            # np.save(os.path.join(simparams.data_dir, filename), pce_valid_score)
+            pce_valid_y_ecdf = uqhelpers.get_exceedance_data(pce_model.y_pred, 1e-5)
+            filename = 'DoE_QuadHem{:d}_{:s}_{:s}_E6R{:d}_y_ecdf.npy'.format(iquad_orders, metamodel_class, pce_fit_method, r)
+            np.save(os.path.join(simparams.data_dir, filename), pce_valid_y_ecdf)
 
 
 
@@ -101,9 +89,9 @@ def main():
             basis = cp.orth_ttr(poly_order,dist_zeta)
             for ia in alpha:
                 ### ============ Get training points ============
-                num_basis= min(int(len(basis)*ia), 1000000)
+                num_basis= min(int(len(basis)*ia), int(1E6))
                 filename = 'DoE_McsE6R{:d}_q{:d}_Opt{:s}{:d}.npy'.format(r, iquad_orders,opt_cri, num_basis)
-                data_set = np.load(os.path.join(simparams.data_dir,'DoE_McsE6_PCE_OLS_OPT', filename))
+                data_set = np.load(os.path.join(simparams.data_dir,'DoE_McsE6_OPT_PCE', filename))
                 print('  > {:<10s}: {:s}'.format('filename', filename))
                 print('    {:<10s}: {}'.format('data shape', data_set.shape))
                 train_u  = data_set[1:3,:] 
@@ -112,11 +100,7 @@ def main():
 
                 ### ============ Get Surrogate Model for each QoI============
                 pce_model = museuq.PCE(poly_order, dist_zeta)
-                # print(len(pce_model.basis[0]))
-                # pce_model.fit(train_u, train_y, w=train_w, fit_method=pce_fit_method)
                 pce_model.fit(train_u, train_y, fit_method=pce_fit_method)
-                # print(pce_model.poly_coeffs)
-                # print(pce_model.basis_coeffs)
 
                 ### ============ Validating surrogate models at training points ============
 
@@ -126,18 +110,16 @@ def main():
                 valid_x  = data_set[2:4,:]
                 valid_y  = data_set[4  ,:]
 
-                pce_valid_y, pce_valid_score = pce_model.predict(valid_u, valid_y, metrics=metrics2cal,prob=upper_tail_probs,moment=moment2cal)
-                pce_valid_y_ecdf = uqhelpers.get_exceedance_data(pce_valid_y, 1e-5)
+                pce_model.predict(valid_u, valid_y, metrics=metrics2cal,prob=upper_tail_probs,moment=moment2cal)
+                filename = 'DoE_McsE6R{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_y.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method)
+                np.save(os.path.join(simparams.data_dir, filename), pce_model.y_pred[0])
 
-                filename = 'DoE_McsE6{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_E6R{:d}_y.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method, r)
-                np.save(os.path.join(simparams.data_dir, filename), pce_valid_y)
+                filename = 'DoE_McsE6R{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_score.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method)
+                np.save(os.path.join(simparams.data_dir, filename), pce_model.scores[0])
 
-                filename = 'DoE_McsE6{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_E6R{:d}_y_ecdf.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method, r)
+                pce_valid_y_ecdf = uqhelpers.get_exceedance_data(pce_model.y_pred, 1e-5)
+                filename = 'DoE_McsE6R{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_y_ecdf.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method)
                 np.save(os.path.join(simparams.data_dir, filename), pce_valid_y_ecdf)
-
-                filename = 'DoE_McsE6{:d}_q{:d}_Opt{:s}{:d}_{:s}_{:s}_E6R{:d}_score.npy'.format(r, iquad_orders, opt_cri, num_basis,metamodel_class, pce_fit_method, r)
-                np.save(os.path.join(simparams.data_dir, filename), pce_valid_score)
-
 
 if __name__ == '__main__':
     main()
