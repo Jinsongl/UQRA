6f38b9c583f2d65defc7d5bc07dba6767661e472
Jinsong <jinsongliu@utexas.edu>
2020-01-11 14:16:48 -0600

    1. LOO error: 	Implementation with closed form leads to large errors (maybe due 	to the inverse of large matrix and large condition number of 	design matrix) 	Using model_selection.cross_val_score instead 	1. model is linear_model.LinearRegression() (Implemented with 	iteration is lsqr instead of closed form) 	2. scoring is negative mse 	3. cv default value is number of samples (leave one out error). 	Could take any other splitting format. need to give n_splits 	4. Multiprocessing: speed up with multiprocessing, n_jobs is the 	number of cpu cores available in the machine 2. LOO calculations are changed both in OLS and OLSLARS
    3. Implement sample_y method: Draw samples from Surrogate model and
    evaluate at X
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   ../surrogate_model/polynomial_chaos_expansion.py

--- museuq/surrogate_model/polynomial_chaos_expansion.py
+++ museuq/surrogate_model/polynomial_chaos_expansion.py
@@ -10,10 +10,11 @@
 
 """
 import numpy as np, chaospy as cp
+import multiprocessing as mp
 from sklearn import linear_model
 from museuq.surrogate_model.base import SurrogateModel
 import museuq.utilities.metrics_collections as uq_metrics 
-
+from sklearn.model_selection import KFold, cross_val_score
 class PolynomialChaosExpansion(SurrogateModel):
     """
     Class to build polynomial chaos expansion (PCE) model
@@ -23,7 +24,6 @@ class PolynomialChaosExpansion(SurrogateModel):
         super().__init__(*args, **kwargs)
         self.dist_zeta_J    = dist_zeta_J
         self.poly_order     = int(poly_order)#### basis_order is just one values (int) 
-
         poly, norm          = cp.orth_ttr(self.poly_order, self.dist_zeta_J, retall=True)
         self.basis_         = poly
         self.basis_norms_   = norm
@@ -108,8 +108,15 @@ class PolynomialChaosExpansion(SurrogateModel):
             self.active_      = range(len(self.basis_))
 
         elif self.fit_method.upper() in ['OLS']:
-            print(r'   * {:<25s} : X = ({},{}), Y = {}'.format('Train data shape', x.shape[1], len(self.basis_), y.shape))
+            X       = self.basis_(*x).T
+            y       = np.squeeze(y)
+            n_splits= kwargs.get('n_splits', X.shape[0])
+            kf      = KFold(n_splits=n_splits,shuffle=True)
+            print(r'   * {:<25s} : X = {}, Y = {}'.format('Train data shape', X.shape, y.shape))
+            model   = linear_model.LinearRegression()
+            neg_mse = cross_val_score(model, X, y, scoring = 'neg_mean_squared_error', cv=kf, n_jobs=mp.cpu_count())
             f_hat, orthpoly_coeffs = cp.fit_regression(self.basis_, x, y, retall=True)
+            self.cv_error     = -np.mean(neg_mse)
             self.metamodels   = f_hat
             self.coeffs_basis_= orthpoly_coeffs
             self.active_      = range(len(self.basis_))
@@ -125,10 +132,10 @@ class PolynomialChaosExpansion(SurrogateModel):
         elif self.fit_method.upper() in ['LASSOLARS']:
             ## parameters for LassoLarsCV
             max_iter= kwargs.get('max_iter', 500)
-            X = self.basis_(*x)
+            X = self.basis_(*x).T
             y = np.squeeze(y)
-            print(r'   * {:<25s} : X = {}, Y = {}'.format('Train data shape', tuple(reversed(X.shape)), y.shape))
-            model = linear_model.LassoLarsCV(max_iter=max_iter, cv=X.shape[1]).fit(X.T,y)
+            print(r'   * {:<25s} : X = {}, Y = {}'.format('Train data shape',X.shape, y.shape))
+            model = linear_model.LassoLarsCV(max_iter=max_iter, cv=X.shape[1]).fit(X,y)
             self.metamodels     = model 
             self.coeffs_basis_  = model.coef_[np.nonzero(model.coef_)] 
             self.active_        = [0,] + list(*np.nonzero(model.coef_))
@@ -138,30 +145,34 @@ class PolynomialChaosExpansion(SurrogateModel):
 
         elif self.fit_method.upper() in ['OLSLARS']:
             """
-            Methods in 
-                Blatman, Géraud, and Bruno Sudret. "Adaptive sparse polynomial chaos expansion based on least angle regression." Journal of Computational Physics 230.6 (2011): 2345-2367.
-
+            Blatman, Géraud, and Bruno Sudret. "Adaptive sparse polynomial chaos expansion based on least angle regression." Journal of Computational Physics 230.6 (2011): 2345-2367.
             """
             ## parameters for LassoLars 
-            cv_error= kwargs.get('error', 'leave_one_out_error')
-            X = self.basis_(*x)
-            y = np.squeeze(y)
-            print(r'   * {:<25s} : X = {}, Y = {}'.format('Train data shape', tuple(reversed(X.shape)), y.shape))
-            model = linear_model.Lars().fit(X.T,y)
-            model_errors  = np.inf
-            for i in range(len(model.active_)+1):
-                active_indices = np.nonzero(model.coef_)[0] #model.active_[:i+1]
-                active_indices = np.array([0, *active_indices])
-                active_basis   = cp.polynomial(self.basis_[active_indices[:i+1]])
-                print(active_basis)
-                f_hat, coeffs  = cp.fit_regression(active_basis, x, y, retall=True)
-                X = active_basis(*x).T
-                error = uq_metrics.leave_one_out_error(X, y)
-                if error < model_errors:
+            X       = self.basis_(*x).T
+            y       = np.squeeze(y)
+            n_splits= kwargs.get('n_splits', X.shape[0])
+            kf      = KFold(n_splits=n_splits,shuffle=True)
+            print(r'   * {:<25s} : X = {}, Y = {}'.format('Train data shape', X.shape, y.shape))
+            model_lars       = linear_model.Lars().fit(X,y)
+            self.active_lars = model_lars.active_
+            self.cv_error    = np.inf
+            for i in range(len(model_lars.active_)):
+                active_indices  = model_lars.active_[:i+1]
+                active_indices  = np.array([0, *active_indices])
+                active_basis    = cp.polynomial(self.basis_[active_indices])
+                X               = active_basis(*x).T
+                model_ols       = linear_model.LinearRegression()
+                neg_mse         = cross_val_score(model_ols, X, y, scoring = 'neg_mean_squared_error', cv=kf, n_jobs=mp.cpu_count())
+                error_loo       = -np.mean(neg_mse)
+                f_hat, coeffs   = cp.fit_regression(active_basis, x, y, retall=True)
+
+                if error_loo < self.cv_error:
                     self.metamodels     = f_hat
                     self.coeffs_basis_  = coeffs
                     self.active_        = active_indices
-            print(r'   * {:<25s} : {}'.format('Active basis', self.active_))
+                    self.cv_error       = error_loo
+
+            print(r'   * {:<25s} : {} ->#:{:d}'.format('Active basis', self.active_, len(self.active_)))
 
         elif self.fit_method.upper() in ['LARS']:
             raise ValueError('Method to calculate PCE coefficients {:s} is not defined'.format(method))
@@ -191,5 +202,27 @@ class PolynomialChaosExpansion(SurrogateModel):
 
         print(r'   * {:<25s} : {}'.format('Prediction output', y_pred.shape))
         return y_pred
+
+    def sample_y(self,X, nsamples=1, random_state=0):
+        """
+        Draw samples from Surrogate model and evaluate at X.
+
+        Parameters:	
+        X : array-like, shape = (n_features,n_samples_X)
+        Query points where the GP samples are evaluated
+
+        nsamples : int, default: 1
+        The number of samples drawn from the Gaussian process
+
+        random_state : int, RandomState instance or None, optional (default=0)
+        If int, random_state is the seed used by the random number generator; If RandomState instance, random_state is the random number generator; If None, the random number generator is the RandomState instance used by np.random.
+
+        Returns:	
+        y_samples : list, list item shape = (n_samples_X, [n_output_dims], nsamples)
+        Values of nsamples samples drawn from Gaussian process and evaluated at query points.
+
+        """
+        raise NotImplementedError
+
 
 
