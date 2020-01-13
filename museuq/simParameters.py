e537cfd7db65f86ec30e60d46c062b9b7fe91716
Jinsong <jinsongliu@utexas.edu>
2020-01-13 11:32:37 -0600

    Add is_adaptive_continue method to simParameters  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   simParameters.py

--- museuq/simParameters.py
+++ museuq/simParameters.py
@@ -38,18 +38,22 @@ class simParameters(object):
         normalize: 
     """
 
-    def __init__(self, model_name, dist_zeta_J):
+    def __init__(self, model_name, dist_zeta_J, **kwargs):
         sys.stdout = Logger()
         ###---------- Random system properties ------------------------
         self.seed       = [0,100]
         self.model_name = model_name
         self.dist_zeta_J= dist_zeta_J   ## Joint distribution
         self.dist_zeta_M= None          ## List of marginal distributions
-        # self.dist_x     = dist_x
-        # self.dist_x_J   = dist_x    #if len(dist_zeta) == 1 else cp.J(*dist_zeta) 
-        # assert len(self.dist_x) == len(self.dist_zeta)
-        # assert len(self.dist_x_J) == len(self.dist_zeta_J)
 
+        ###------------- Adaptive setting -----------------------------
+        self.n_budget   = kwargs.get('n_budget' , None  )
+        self.r2_bound   = kwargs.get('r2_bound' , 0.9   )
+        self.mse_bound  = kwargs.get('mse_bound', None  )
+        self.mse_diff   = kwargs.get('mse_diff' , 0.05  ) 
+        self.plim       = kwargs.get('plim'     , (0, 15))
+        self.qdiff_bound= kwargs.get('mquantiles', 0.05  ) 
+        
         ###-------------Directories setting -----------------------------
         self.pwd            = ''
         self.data_dir       = ''
@@ -96,6 +100,15 @@ class simParameters(object):
         self.data_dir   = kwargs.get('data_dir'     , data_dir      )
         self.figure_dir = kwargs.get('figure_dir'   , figure_dir    )
         self.data_dir_id= kwargs.get('data_dir_id'  , data_dir_id   )
+
+    def set_adaptive_parameters(self, **kwargs):
+
+        self.n_budget   = kwargs.get('n_budget' , np.inf)
+        self.r2_bound   = kwargs.get('r2_bound' , 0.9   )
+        self.mse_bound  = kwargs.get('mse_bound', None  )
+        self.mse_diff   = kwargs.get('mse_diff' , 0.05  ) 
+        self.plim       = kwargs.get('plim'     , (0, 15))
+        self.qdiff_bound= kwargs.get('q_bound'  , 0.05  ) 
 
     def set_params(self, **kwargs):
         """
@@ -158,7 +171,6 @@ class simParameters(object):
             print(r'     - {:<15s} : {} '.format('qoi2analysis', qoi2analysis))
             stats_list = ['mean', 'std', 'skewness', 'kurtosis', 'absmax', 'absmin', 'up_crossing']
             print(r'     - {:<15s} : {} '.format('statistics'  , list(compress(stats_list, self.stats)) ))
-    
 
     def _make_output_dir(self, model_name):
         """
@@ -204,8 +216,6 @@ class simParameters(object):
             pass
         return MODEL_DIR_DATA_ID, DATA_DIR, FIGURE_DIR
 
-
-    
     def _get_gdrive_folder_id(self, folder_name):
         """
         Check if the given folder_name exists in Google Drive. 
@@ -225,5 +235,135 @@ class simParameters(object):
         command = 'gdrive list --order folder |grep ' +  folder_name
         folder_id = os.popen(command).read()
         return folder_id[:33]
+
+
+    def is_adaptive_continue(self, nsim, **kwargs):
+        """
+        Stopping criteria for adaptive algorithm
+            Algorithm will have a hard stop (return False) when either of following occurs:
+                1. nsim >= n_budget
+                2. for PCE, poly_order > plim[-1]
+        Arguments:
+            nsim: number of evaluations has been done (should be <= self.n_budget)
+        Optional:
+            poly_order: for PCE model, polynomial order (should be in range self.plim) 
+
+        Return:
+            Bool
+            return true when the algorithm should continue. i.e.
+                1. hard stop on n_budget and poly_order not met 
+                2. at least one of the given metric criteria is NOT met
+        """
+
+        print('==> Checking adaptive conditions...')
+        is_adaptive = []
+        ## Algorithm stop when nsim >= n_budget 
+        if nsim > self.n_budget:
+            print(' => Stopping... Reach simulation budget < {:d} >= {:d} >'.format(nsim, self.n_budget))
+            return False
+
+        ## Algorithm stop when poly_order > self.plim[-1]
+        ## If poly_order is not given, this criteria should not affect the running of the algorithm. Return True
+        poly_order = kwargs.pop('poly_order', -np.inf)
+        if poly_order > self.plim[1]:
+            print(' => Stopping... Exceed max polynomial order p({:d}) > {:d}'.format(poly_order, self.plim[1]))
+            return False
+
+        cv_error = kwargs.pop('cv_error', None)
+        if (cv_error is None) or (not cv_error) or (len(cv_error) < 3):
+            pass
+        else:
+            cv_error = np.array(cv_error)
+            if (cv_error[-3] < cv_error[-2]) and (cv_error[-2] < cv_error[-1]):
+                print(' => Stopping... Overfitting detected: {}'.format( np.around(cv_error, 4)))
+                return False
+
+        ### For following metrics, algorithm stop (False) when all of these met.
+        ### i.e. Algorithm continue (True) when at least one of these metrics not met
+        is_metrics = []
+
+
+        ## Algorithm continue when r2 <= r2_bound (NOT met, return True)
+        r2    = kwargs.pop('r2', None)
+        if r2 is None:
+            is_r2 = False
+        elif (not r2) or (len(r2) < 2): ## [r2 is empty (initial step), not defined, not engouth data]
+            is_r2 = True
+        else:
+            r2    = np.array(r2)
+            is_r2 = r2 < self.r2_bound
+            is_r2 = is_r2[-2:]
+            is_r2 = is_r2.any()
+            if not is_r2:
+                print('  * Condition met: < {:<15s}: {} >'.format('R-squred', np.squeeze(r2[-2:])))
+        is_metrics.append(is_r2)
+
+        ## Algorithm continue when r2_adj <= r2_bound (NOT met, return True)
+        r2_adj = kwargs.pop('r2_adj', None)
+        if r2_adj is None:
+            is_r2_adj = False
+        elif (not r2_adj)  or (len(r2_adj) < 2): ## [r2_adj is empty (initial step), not defined, not engouth data]
+            is_r2_adj = True
+        else:
+            r2_adj    = np.array(r2_adj)
+            is_r2_adj = r2_adj < self.r2_bound
+            is_r2_adj = is_r2_adj[-2:]
+            is_r2_adj = is_r2_adj.any()
+            if not is_r2_adj:
+                print('  * Condition met: < {:<15s}: {} >'.format('Adjusted R2', np.squeeze(r2_adj[-2:])))
+        is_metrics.append(is_r2_adj)
+
+        ## Algorithm continue when mse continue when mse > mse_bound(NOT met, return True)
+        mse    = kwargs.pop('mse', None)
+        if mse is None:
+            is_mse = False
+        elif not mse:
+            is_mse = True
+        else:
+            mse    = np.array(mse)
+            is_mse = mse >= self.mse_bound 
+            is_mse = is_mse[-2:]
+            is_mse = is_mse.any()
+            if not is_mse:
+                print('  * Condition met: < {:<15s}: {} >'.format('MSE ', np.squeeze(mse[-2:])))
+        is_metrics.append(is_mse)
+
+        ## Algorithm continue when mse_diff continue when mse_diff > self.mse_diff(NOT met, return True)
+        if mse is None:
+            is_mse_diff = False
+        elif (not mse) or (len(mse) < 3): ## no mse is given or empty for initial step
+            is_mse_diff = True
+        else:
+            mse      = np.array(mse)
+            mse_diff = abs((mse[1:] - mse[:-1])/mse[:-1])
+            is_mse_diff = mse_diff > self.mse_diff
+            is_mse_diff = is_mse_diff[-2:]
+            is_mse_diff = is_mse_diff.any()
+            if not is_mse_diff:
+                print('  * Condition met: < {:<15s}: {} >'.format('MSE diff', np.squeeze(mse_diff[-2:])))
+        is_metrics.append(is_mse_diff)
+
+        ## Algorithm stop when mquantiles continue when qdiff > self.qdiff_bound
+        mquantiles = kwargs.pop('mquantiles', None)
+        if mquantiles is None:
+            is_qdiff = False
+        elif (not mquantiles) or (len(mquantiles) < 3):
+            is_qdiff = True
+        else:
+            mquantiles = np.array(mquantiles)
+            qdiff    = abs((mquantiles[1:] - mquantiles[:-1])/mquantiles[:-1])
+            is_qdiff = qdiff > self.qdiff_bound
+            is_qdiff = is_qdiff[-2:]
+            is_qdiff = is_qdiff.any()
+            if not is_qdiff:
+                print('  * Condition met: < {:<15s}: {} >'.format('mquantiles diff', np.squeeze(qdiff[-2:])))
+        is_metrics.append(is_qdiff)
+        ### If any above metric is True ( NOT met), algorithm will continue (return True)
+
+        if not kwargs:
+            is_adaptive = np.array(is_metrics).any()
+            return is_adaptive
+        else:
+            raise ValueError('Given stopping criteria {} not defined'.format(kwargs.keys()))
 
 
