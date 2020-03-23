3226b1bb0e2a65f21c7043daec6fab6937f6e870
Jinsong <jinsongliu@utexas.edu>
2020-03-23 01:32:58 -0500

    change to run in batch size. too long to urn  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   ../examples/duffing/run_duffing.py

--- examples/duffing/run_duffing.py
+++ examples/duffing/run_duffing.py
@@ -24,15 +24,18 @@ def main():
     print(solver)
     data_dir_src    = '/Volumes/GoogleDrive/My Drive/MUSE_UQ_DATA/Samples/Kvitebjorn/Normal/'
     data_dir_destn  = r'/Volumes/External/MUSE_UQ_DATA/Duffing/Data/' 
-    for r in range(1):
+    batch_size = int(10000)
+    for i in range(0,1):
+        batch_start = (i)*batch_size
+        batch_end   = (i+1)*batch_size
         data = []
-        filename = 'DoE_McsE6R{:d}.npy'.format(r)
-        x = np.load(os.path.join(data_dir_src, filename))[:solver.ndim, :]
+        filename = 'DoE_McsE6R{:d}.npy'.format(0)
+        x = np.load(os.path.join(data_dir_src, filename))[:solver.ndim, batch_start:batch_end]
         # x = solver.map_domain(u, [stats.norm(0,1),] * solver.ndim) 
         # y_raw, y_QoI = zip(*[solver.run(x.T) for _ in range(nsim)]) 
         y_raw, y_QoI = solver.run(x.T)
         # np.save('duffing_time_series_{:d}'.format(r), y_raw)
-        filename = 'DoE_McsE6R{:d}_stats'.format(r)
+        filename = 'DoE_McsE6R0_{:d}_stats'.format(i)
         np.save(os.path.join(data_dir_destn, filename), y_QoI)
 
     # np.random.seed(100)
