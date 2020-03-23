9fff8a13b15200c1593f09a7a3e6d9f06c2c7181
Jinsong <jinsongliu@utexas.edu>
2020-03-23 01:27:33 -0500

    file to run duffing  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   examples/duffing/run_duffing.py

---
+++ examples/duffing/run_duffing.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Jinsong Liu <jinsongliu@utexas.edu>
+#
+# Distributed under terms of the GNU-License license.
+
+"""
+
+"""
+import numpy as np
+import museuq
+import os
+
+def main():
+
+    np.random.seed(100)
+    dt = 0.1 
+    qoi2analysis = [1,2]
+    nsim = 1
+    stats2cal = ['mean', 'std', 'skewness', 'kurtosis', 'absmax', 'absmin']
+    solver = museuq.duffing_oscillator(qoi2analysis=qoi2analysis, stats2cal=stats2cal, tmax=1000, dt=dt,y0=[1,0], spec_name='JONSWAP')
+    print(solver)
+    data_dir_src    = '/Volumes/GoogleDrive/My Drive/MUSE_UQ_DATA/Samples/Kvitebjorn/Normal/'
+    data_dir_destn  = r'/Volumes/External/MUSE_UQ_DATA/Duffing/Data/' 
+    for r in range(1):
+        data = []
+        filename = 'DoE_McsE6R{:d}.npy'.format(r)
+        x = np.load(os.path.join(data_dir_src, filename))[:solver.ndim, :]
+        # x = solver.map_domain(u, [stats.norm(0,1),] * solver.ndim) 
+        # y_raw, y_QoI = zip(*[solver.run(x.T) for _ in range(nsim)]) 
+        y_raw, y_QoI = solver.run(x.T)
+        # np.save('duffing_time_series_{:d}'.format(r), y_raw)
+        filename = 'DoE_McsE6R{:d}_stats'.format(r)
+        np.save(os.path.join(data_dir_destn, filename), y_QoI)
+
+    # np.random.seed(100)
+    # qoi2analysis = [1,2]
+    # stats2cal = ['mean', 'std', 'skewness', 'kurtosis', 'absmax', 'absmin']
+    # solver = museuq.linear_oscillator(qoi2analysis=qoi2analysis, stats2cal=stats2cal)
+    # print()
+    # data_dir = '/Volumes/GoogleDrive/My Drive/MUSE_UQ_DATA/Samples/Kvitebjorn/Normal/'
+    # for r in range(10):
+        # data = []
+        # filename = 'DoE_McsE6R{:d}.npy'.format(r)
+        # x = np.load(os.path.join(data_dir, filename))
+        # # x = solver.map_domain(u, [stats.norm(0,1),] * solver.ndim) 
+        # y_raw, y_QoI = solver.run(x.T) 
+        # print(np.array(y_QoI).shape)
+
+if __name__ == '__main__':
+    main()
