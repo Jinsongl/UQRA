cb8ab7e3bfe86becc17c1726b82d9e014d23f9b3
Jinsong <jinsongliu@utexas.edu>
2020-05-01 18:55:33 -0500

    Sparse PCE procedure changed
    1. samples with CLS/MCS
    2. Number of new samples based on var() > 0.95
    3. new samples based on all basis with |coef|>1e-6. Used to be var>0.95
       which excluded a lot terms with small coefficients but has
       significant impact on high quantile values
       In this step, one can apply optimality methods
    4. OLS for basis with |coef|>1e-6 with all samples

--- examples/nonadaptive_sparse_PCE.py
+++ examples/nonadaptive_sparse_PCE.py
@@ -21,43 +21,39 @@ sys.stdout  = museuq.utilities.classes.Logger()
 def main():
 
     ## ------------------------ Displaying set up ------------------- ###
-    np.set_printoptions(precision=4)
-    np.set_printoptions(threshold=8)
+    np.set_printoptions(precision=8)
+    np.set_printoptions(threshold=1000)
     np.set_printoptions(suppress=True)
-    pf          = [1e-4, 1e-5, 1e-6]
+    pf          = [1e-4, 1e-5]
     np.random.seed(100)
     ## ------------------------ Define solver ----------------------- ###
-    # orth_poly   = museuq.Legendre(d=ndim, deg=p)
-    # orth_poly   = museuq.Hermite(d=ndim, deg=p, hem_type='physicists')
-    # orth_poly   = museuq.Hermite(d=ndim, deg=p, hem_type='probabilists')
-    # solver      = museuq.SparsePoly(orth_poly, sparsity='full', seed=100)
-
-    # solver      = museuq.ExpAbsSum(stats.uniform(-1,2),d=2,c=[-2,1],w=[0.25,-0.75])
+    solver      = museuq.ExpAbsSum(stats.uniform(-1,2),d=2,c=[-2,1],w=[0.25,-0.75])
     # solver      = museuq.ExpSquareSum(stats.uniform(-1,2),d=2,c=[1,1],w=[1,0.5])
     # solver      = museuq.CornerPeak(stats.uniform(-1,2), d=2)
     # solver      = museuq.ProductPeak(stats.uniform(-1,2), d=2,c=[-3,2],w=[0.5,0.5])
     # solver      = museuq.Franke()
     # solver      = museuq.Ishigami()
 
-    solver      = museuq.ExpAbsSum(stats.norm(0,1),d=2,c=[-2,1],w=[0.25,-0.75])
+    # solver      = museuq.ExpAbsSum(stats.norm(0,1),d=2,c=[-2,1],w=[0.25,-0.75])
     # solver      = museuq.ExpSquareSum(stats.norm(0,1),d=2,c=[1,1],w=[1,0.5])
     # solver      = museuq.CornerPeak(stats.norm(0,1), d=3, c=np.array([1,2,3]), w=[0.5,]*3)
     # solver      = museuq.ProductPeak(stats.norm(0,1), d=2, c=[-3,2], w=[0.5,]*2)
     # solver      = museuq.ExpSum(stats.norm(0,1), d=3)
     # solver      = museuq.FourBranchSystem()
     ## ------------------------ Simulation Parameters ----------------- ###
-    simparams = museuq.Parameters(solver)
-    simparams.pce_degs   = np.array(range(2,7))
+    simparams = museuq.Parameters()
+    simparams.solver     = solver
+    simparams.pce_degs   = np.array([5])
     simparams.n_cand     = int(1e5)
     simparams.n_test     = -1
-    simparams.doe_method = 'MCS' ### 'mcs', 'D', 'S', 'reference'
-    simparams.optimality = 'S'#'D', 'S', None
+    simparams.doe_method = 'CLS' ### 'mcs', 'D', 'S', 'reference'
+    simparams.optimality = None #'D', 'S', None
     # simparams.hem_type   = 'physicists'
-    simparams.hem_type   = 'probabilists'
+    # simparams.hem_type   = 'probabilists'
     simparams.fit_method = 'LASSOLARS'
     simparams.n_splits   = 50
     repeats              = 50 if simparams.optimality is None else 1
-    alphas               = np.arange(3,11)/10 
+    alphas               = [0.7] 
     # alphas               = [-1]
     # simparams.num_samples=np.arange(21+1, 130, 5)
     simparams.update()
@@ -74,8 +70,8 @@ def main():
         print('     - {:<23s} : {}'.format('Optimality '      , simparams.optimality))
         print('     - {:<23s} : {}'.format('Fitting method'   , simparams.fit_method))
         ## ----------- Define PCE  ----------- ###
-        # orth_poly= museuq.Legendre(d=solver.ndim, deg=p)
-        orth_poly= museuq.Hermite(d=solver.ndim, deg=p, hem_type=simparams.hem_type)
+        orth_poly= museuq.Legendre(d=solver.ndim, deg=p)
+        # orth_poly= museuq.Hermite(d=solver.ndim, deg=p, hem_type=simparams.hem_type)
         pce_model= museuq.PCE(orth_poly)
         pce_model.info()
 
@@ -86,6 +82,8 @@ def main():
         print(' > Getting candidate data set...')
         u_cand = modeling.get_candidate_data()
         u_test, x_test, y_test = modeling.get_test_data(solver, pce_model) 
+        print(museuq.metrics.mquantiles(y_test, 1-np.array(pf)))
+        u_cand_p = p ** 0.5 * u_cand if modeling.is_cls_unbounded() else u_cand
         # assert np.array_equal(u_test, x_test)
         with np.printoptions(precision=2):
             u_cand_mean_std = np.array((np.mean(u_cand[0]), np.std(u_cand[0])))
@@ -103,115 +101,124 @@ def main():
 
         ## ----------- Oversampling ratio ----------- ###
         simparams.update_num_samples(pce_model.num_basis, alphas=alphas)
-        print(' > Oversampling ratio: {}'.format(np.around(simparams.alphas,2)))
+        print(' > Starting with oversampling ratio {}'.format(np.around(simparams.alphas,2)))
 
-        QoI_nsample     = []
-        score_nsample   = []
-        cv_err_nsample  = []
-        test_err_nsample= []
-        coef_err_nsample= []
-        cond_num_nsample= []
-        u_train = [None,] * repeats
-        for i, n in enumerate(simparams.num_samples):
-            ### ============ Initialize pce_model for each n ============
-            pce_model= museuq.PCE(orth_poly)
-            ### ============ Get training points ============
-            u_cand_p = p ** 0.5 * u_cand if modeling.is_cls_unbounded() else u_cand
-            # n = n - len(modeling.sample_selected)
-            u_train = u_train[0] if repeats == 1 else u_train
-            if simparams.optimality is not None:
-                n1 = max(2,int(round(n/2)))
-            else:
-                n1 = n
-            _, u_train = modeling.get_train_data((repeats,n1), u_cand_p, u_train=None, basis=pce_model.basis)
-            # print(modeling.sample_selected)
-            QoI_repeat     = []
-            score_repeat   = []
-            cv_err_repeat  = []
-            test_err_repeat= []
-            cond_num_repeat= []
-            coef_err_repeat= []
-            u_train  = [u_train,] if repeats == 1 else u_train
-            for iu_train in tqdm(u_train, ascii=True, ncols=80,
-                    desc='   [alpha={:.2f}, {:d}/{:d}, n={:d}]'.format(simparams.alphas[i], i+1, len(simparams.alphas),n)):
-
-                ix_train = solver.map_domain(iu_train, pce_model.basis.dist_u)
-                # assert np.array_equal(iu_train, ix_train)
-                iy_train = solver.run(ix_train)
-                if simparams.optimality is not None:
-                    ### ============ Build Full PCE model ============
-                    U_train = pce_model.basis.vandermonde(iu_train)[:, modeling.active_index] 
-                    if simparams.doe_method.lower().startswith('cls'):
-                        w_train = modeling.cal_cls_weight(iu_train, pce_model.basis)
-                        U_train = modeling.rescale_data(U_train, w_train) 
-                    else:
-                        w_train = None
-                        U_train = U_train
-
-                    pce_model.fit(simparams.fit_method, iu_train, iy_train, w_train, n_splits=simparams.n_splits)
-                    pce_model.estimate_sparsity_var(0.95)
-
-                    ### update candidate data set for this p degree, cls unbuounded
-                    u_train_new, _ = modeling.get_train_data(n-n1, u_cand_p, u_train=iu_train, basis=pce_model.basis, active_basis=pce_model.var_basis)
-                    x_train_new = solver.map_domain(u_train_new, pce_model.basis.dist_u)
-                    y_train_new = solver.run(x_train_new)
-                    iu_train = np.hstack((iu_train, u_train_new)) 
-                    ix_train = np.hstack((ix_train, x_train_new)) 
-                    iy_train = np.hstack((iy_train, y_train_new)) 
-
+        random_idx = np.random.randint(0, u_cand_p.shape[1], size=(repeats, simparams.num_samples[-1]))
+        u_train = [u_cand_p[:, idx] for idx in random_idx] 
+        x_train = [solver.map_domain(iu_train, pce_model.basis.dist_u) for iu_train in u_train]
+        y_train = [solver.run(ix_train) for ix_train in x_train]
+        nsamples= [[iu_train.shape[1],] for iu_train in u_train]
+        # for i, n in enumerate(simparams.num_samples):
+        QoI_repeat     = []
+        score_repeat   = []
+        cv_err_repeat  = []
+        test_err_repeat= []
+        cond_num_repeat= []
+        coef_err_repeat= []
+        for i in tqdm(range(repeats), ascii=True, ncols=80):
+            tqdm.write('------------------------------ Resampling: {:d}/{:d} ------------------------------'.format(i, repeats))
+            iu_train = u_train[i]
+            ix_train = x_train[i]
+            iy_train = y_train[i]
+            QoI_nsample     = []
+            score_nsample   = []
+            cv_err_nsample  = []
+            test_err_nsample= []
+            coef_err_nsample= []
+            cond_num_nsample= []
+            while nsamples[i][-1] < math.ceil(1.1*pce_model.num_basis):
                 ### ============ Build Surrogate Model ============
-                U_train = pce_model.basis.vandermonde(iu_train)[:, modeling.active_index]
+                if simparams.doe_method.lower().startswith('cls'):
+                    w_train = modeling.cal_cls_weight(iu_train, pce_model.basis)
+                else:
+                    w_train = None
+                pce_model.fit('LASSOLARS', iu_train, iy_train, w_train, n_splits=simparams.n_splits)
+                ### update candidate data set for this p degree, cls unbuounded
+                # n = min(pce_model.sparsity, math.ceil(1.1*pce_model.num_basis) - iu_train.shape[1])
+                # if n == 0:
+                    # break
+                pce_model.var(0.95)
+                n = min(len(pce_model.var_pct_basis), math.ceil(1.1*pce_model.num_basis) - nsamples[i][-1])
+                u_train_new, _ = modeling.get_train_data(n, u_cand_p, u_train=iu_train, basis=pce_model.basis, active_basis=pce_model.active_basis)
+                # u_train_new, _ = modeling.get_train_data(n, u_cand_p, u_train=iu_train, basis=pce_model.basis)
+                x_train_new = solver.map_domain(u_train_new, pce_model.basis.dist_u)
+                y_train_new = solver.run(x_train_new)
+                iu_train = np.hstack((iu_train, u_train_new)) 
+                ix_train = np.hstack((ix_train, x_train_new)) 
+                iy_train = np.hstack((iy_train, y_train_new)) 
+                u_train[i] = iu_train
+                x_train[i] = ix_train
+                y_train[i] = iy_train
+                nsamples[i].append(iu_train.shape[-1])
+
+                U_train = pce_model.basis.vandermonde(iu_train)[:, modeling.active_index] 
                 if simparams.doe_method.lower().startswith('cls'):
                     w_train = modeling.cal_cls_weight(iu_train, pce_model.basis)
                     U_train = modeling.rescale_data(U_train, w_train) 
                 else:
                     w_train = None
                     U_train = U_train
-
-                pce_model.fit('ols', iu_train, iy_train, w_train, n_splits=simparams.n_splits, active_basis=pce_model.var_basis)
-                # pce_model.fit(simparams.fit_method, iu_train, y_train, w_train)
-                # pce_model.fit_ols(iu_train, y_train, w_train)
+                tqdm.write('   [alpha={:.2f}, n={:d}]'.format(nsamples[i][-1]/pce_model.num_basis, nsamples[i][-1]))
+                pce_model.fit('ols', iu_train, iy_train, w_train, n_splits=simparams.n_splits, active_basis=pce_model.active_basis)
+                # pce_model.fit('ols', iu_train, iy_train, w_train, n_splits=simparams.n_splits)
                 y_train_hat = pce_model.predict(iu_train)
                 y_test_hat  = pce_model.predict(u_test)
-
                 ## condition number, kappa = max(svd)/min(svd)
                 _, sig_value, _ = np.linalg.svd(U_train)
                 kappa = max(abs(sig_value)) / min(abs(sig_value)) 
 
+                QoI_nsample.append(museuq.metrics.mquantiles(y_test_hat, 1-np.array(pf)))
+                test_err_nsample.append(museuq.metrics.mean_squared_error(y_test, y_test_hat))
+                cond_num_nsample.append(kappa)
+                score_nsample.append(pce_model.score)
+                cv_err_nsample.append(pce_model.cv_error)
 
-                # QoI_repeat.append(np.linalg.norm(solver.coef- pce_model.coef, np.inf) < 1e-2)
-                QoI_repeat.append(museuq.metrics.mquantiles(y_test_hat, 1-np.array(pf)))
-                test_err_repeat.append(museuq.metrics.mean_squared_error(y_test, y_test_hat))
-                cond_num_repeat.append(kappa)
-                score_repeat.append(pce_model.score)
-                cv_err_repeat.append(pce_model.cv_error)
+                ### ============ calculating & updating metrics ============
+                with np.printoptions(precision=4):
+                    tqdm.write('     - {:<15s} : {}'.format( 'QoI'       , QoI_nsample[-1]))
+                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'Test MSE ' , test_err_nsample[-1]))
+                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'CV MSE'    , cv_err_nsample[-1]))
+                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'Score '    , score_nsample[-1]))
+                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'kappa '    , cond_num_nsample[-1]))
+                    tqdm.write('     ----------------------------------------')
 
-            ### ============ calculating & updating metrics ============
-            with np.printoptions(precision=4):
-                print('     - {:<15s} : {}'.format( 'QoI'       , np.mean(QoI_repeat, axis=0)))
-                print('     - {:<15s} : {:.4f}'.format( 'Test MSE ' , np.mean(test_err_repeat)))
-                print('     - {:<15s} : {:.4f}'.format( 'CV MSE'    , np.mean(cv_err_repeat)))
-                print('     - {:<15s} : {:.4f}'.format( 'Score '    , np.mean(score_repeat)))
-                print('     - {:<15s} : {:.4f}'.format( 'kappa '    , np.mean(cond_num_repeat)))
-                print('     ----------------------------------------')
+            # QoI_repeat.append(np.linalg.norm(solver.coef- pce_model.coef, np.inf) < 1e-2)
+            QoI_repeat.append(np.array(QoI_nsample))
+            test_err_repeat.append(test_err_nsample)
+            cond_num_repeat.append(cond_num_nsample)
+            score_repeat.append(score_nsample)
+            cv_err_repeat.append(cv_err_nsample)
 
-            QoI_nsample.append(np.array(QoI_repeat))
-            test_err_nsample.append(test_err_repeat)
-            cv_err_nsample.append(cv_err_repeat)
-            score_nsample.append(score_repeat)
-            cond_num_nsample.append(cond_num_repeat)
-        QoI_nsample      = np.moveaxis(np.array(QoI_nsample), -1, 0)
-        QoI_nsample      = [iqoi for iqoi in QoI_nsample]
-        score_nsample    = np.array(score_nsample)
-        test_err_nsample = np.array(test_err_nsample)
-        cond_num_nsample = np.array(cond_num_nsample)
-        poly_deg = score_nsample/score_nsample*p
-        nsamples = np.repeat(simparams.num_samples.reshape(-1,1), score_nsample.shape[1], axis=1)
+            # print(QoI_repeat.shape)
+            # print(test_err_repeat.shape)
+            # print(cond_num_repeat.shape)
+            # print(score_repeat.shape)
+            # print(cv_err_repeat.shape)
+        # QoI_nsample      = np.moveaxis(np.array(QoI_nsample), -1, 0)
+        # QoI_nsample      = [iqoi for iqoi in QoI_nsample]
+        # score_nsample    = np.array(score_nsample)
+        # test_err_nsample = np.array(test_err_nsample)
+        # cond_num_nsample = np.array(cond_num_nsample)
+        # poly_deg         = score_nsample/score_nsample*p
+        # nsamples         = np.array(nsamples) 
+        # data_alpha       = np.array([poly_deg, nsamples, *QoI_nsample, cond_num_nsample, score_nsample, test_err_nsample])
+        # data_alpha       = np.moveaxis(data_alpha, 1, 0)
 
-        data_alpha = np.array([poly_deg, nsamples, *QoI_nsample, cond_num_nsample, score_nsample, test_err_nsample])
-        data_alpha = np.moveaxis(data_alpha, 1, 0)
+
+        print(np.array(QoI_repeat).shape)
+        QoI = np.moveaxis(np.array(QoI_repeat), 0, -1)
+        QoI = np.moveaxis(QoI, 0, 1)
+        QoI = [iqoi for iqoi in QoI]
+        score    = np.array(score_repeat).T
+        test_err = np.array(test_err_repeat).T
+        cond_num = np.array(cond_num_repeat).T
+        poly_deg = score/score*p
+        nsamples = np.array(nsamples).T
+        data_alpha = np.array([poly_deg, nsamples, *QoI, cond_num, score, test_err])
+        # data_alpha       = np.moveaxis(data_alpha, 1, 0)
+
         data_poly_deg.append(data_alpha)
-    filename = '{:s}_{:s}_{:s}'.format(solver.nickname, pce_model.tag, simparams.tag)
+    filename = '{:s}_{:s}_{:s}_test'.format(solver.nickname, pce_model.tag, simparams.tag)
     try:
         np.save(os.path.join(simparams.data_dir_result, filename), np.array(data_poly_deg))
     except:
