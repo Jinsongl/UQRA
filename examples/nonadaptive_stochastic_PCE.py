dfc26592dd5e383a6c7110a356d3eccdee48efe6
Jinsong <jinsongliu@utexas.edu>
2020-05-24 20:53:20 -0500

    update

--- examples/nonadaptive_stochastic_PCE.py
+++ examples/nonadaptive_stochastic_PCE.py
@@ -24,11 +24,12 @@ def main():
     np.set_printoptions(threshold=1000)
     np.set_printoptions(suppress=True)
     pf = [1e-4, 1e-5, 1e-6]
+    test_filename = r'DoE_McsE6R8.npy'
     ## ------------------------ Define solver ----------------------- ###
     random_seed = 100
     out_responses = [2]
     out_stats = ['absmax']
-    n_short_term = 10
+    n_short_term = 1
     m=1
     c=0.1/np.pi
     k=1.0/np.pi/np.pi
@@ -36,26 +37,24 @@ def main():
     # env    = museuq.Environment([stats.uniform, stats.norm])
     # env    = museuq.environment.Kvitebjorn.Kvitebjorn()
     env    = museuq.Environment([2,])
-    np.random.seed(random_seed)
-    seeds_st = np.random.randint(0, int(2**31-1), size=n_short_term)
     solver = museuq.linear_oscillator(m=m,c=c,k=k,excitation='spec_test1', environment=env,
-            t=1000,t_transit=10, dt=0.1, out_responses=out_responses, out_stats=out_stats, seeds_st=seeds_st)
+            t=1000,t_transit=10, dt=0.1, out_responses=out_responses, out_stats=out_stats, phase=range(n_short_term))
     ## ------------------------ Simulation Parameters ----------------- ###
     simparams = museuq.Parameters()
     simparams.solver     = solver
-    simparams.pce_degs   = np.array(range(2,3))
+    simparams.pce_degs   = np.array(range(10,11))
     simparams.n_cand     = int(1e5)
     simparams.n_test     = -1
     simparams.doe_method = 'CLS' ### 'mcs', 'D', 'S', 'reference'
-    simparams.optimality = None #'D', 'S', None
+    simparams.optimality = 'S'#'D', 'S', None
     simparams.hem_type   = 'physicists'
     # simparams.hem_type   = 'probabilists'
     simparams.fit_method = 'OLS'
     simparams.n_splits   = 50
     repeats              = 1 if simparams.optimality is None else 1
     # alphas               = np.arange(3,11)/10 
-    # alphas               = [1.2]
-    alphas               = [-1]
+    alphas               = [1.2]
+    # alphas               = [-1]
     # simparams.num_samples=np.arange(21+1, 130, 5)
     simparams.update()
     simparams.info()
@@ -82,11 +81,10 @@ def main():
         ## ----------- Candidate and testing data set for DoE ----------- ###
         print(' > Getting candidate data set...')
         u_cand = modeling.get_candidate_data()
-        u_test, x_test, y_test = modeling.get_test_data(solver, pce_model, filename=r'DoE_McsE6R0.npy', qoi=out_responses,n=1e2) 
+        u_test, x_test, y_test = modeling.get_test_data(solver, pce_model, filename=test_filename, qoi=out_responses,n=1e6) 
         y_test_mean = np.mean(y_test, axis=0)
         y_test_std  = np.std(y_test, axis=0)
         print(museuq.metrics.mquantiles(y_test.T, 1-np.array(pf), multioutput='raw_values'))
-        print('y_test shape:{}'.format(y_test.shape))
         u_cand_p = p ** 0.5 * u_cand if modeling.is_cls_unbounded() else u_cand
         # assert np.array_equal(u_test, x_test)
         with np.printoptions(precision=2):
@@ -139,58 +137,66 @@ def main():
 
                 pce_model.fit(simparams.fit_method,iu_train,iy_train.T,w_train,n_splits=simparams.n_splits)
                 y_test_hat  = pce_model.predict(u_test)
+                y_pred = np.vstack((u_test, x_test, y_test_hat.reshape(1,-1)))
+                print(u_test.shape)
+                print(x_test.shape)
+                print(y_test_hat.shape)
+                np.save(os.path.join(simparams.data_dir_result,'TestData', modeling.filename_test[:-4]+'_y2_pred.npy'), y_pred)
+                
+                # pce_model.fit(simparams.fit_method,iu_train,iy_train_mean,w_train,n_splits=simparams.n_splits)
+                # y_test_mean_hat  = pce_model.predict(u_test)
 
-                pce_model.fit(simparams.fit_method,iu_train,iy_train_mean,w_train,n_splits=simparams.n_splits)
-                y_test_mean_hat  = pce_model.predict(u_test)
+                # pce_model.fit(simparams.fit_method,iu_train,iy_train_std,w_train,n_splits=simparams.n_splits)
+                # y_test_std_hat  = pce_model.predict(u_test)
 
-                pce_model.fit(simparams.fit_method,iu_train,iy_train_std,w_train,n_splits=simparams.n_splits)
-                y_test_std_hat  = pce_model.predict(u_test)
+                # # y_pred = np.vstack((y_test_hat.reshape(-1,n_test), y_test_mean_hat.reshape(-1,n_test), y_test_std.reshape(-1,n_test)))
 
-                test_mse = museuq.metrics.mean_squared_error(y_test.T, y_test_hat,multioutput='raw_values')
-                test_mean_mse = museuq.metrics.mean_squared_error(y_test_mean, y_test_mean_hat)
-                test_std_mse = museuq.metrics.mean_squared_error(y_test_std, y_test_std_hat)
-                excd_vals = museuq.metrics.mquantiles(y_test_hat, 1-np.array(pf), multioutput='raw_values')
-                excd_mean_val = museuq.metrics.mquantiles(y_test_mean_hat, 1-np.array(pf), multioutput='raw_values')
-                data = np.array([p, n, kappa, pce_model.score, pce_model.cv_error])
-                data = np.append(data, test_mse)
-                data = np.append(data, test_mean_mse)
-                data = np.append(data, test_std_mse)
-                for iexcd_val, iexcd_mean_val in zip(excd_vals, excd_mean_val):
-                    data = np.append(data, iexcd_val)
-                    data = np.append(data, iexcd_mean_val)
-                data_poly_deg.append(data)
+                # test_mse = museuq.metrics.mean_squared_error(y_test.T, y_test_hat,multioutput='raw_values')
+                # test_mean_mse = museuq.metrics.mean_squared_error(y_test_mean, y_test_mean_hat)
+                # test_std_mse = museuq.metrics.mean_squared_error(y_test_std, y_test_std_hat)
+                # excd_vals = museuq.metrics.mquantiles(y_test_hat, 1-np.array(pf), multioutput='raw_values').reshape(len(pf), -1)
+                # excd_mean_val = museuq.metrics.mquantiles(y_test_mean_hat, 1-np.array(pf), multioutput='raw_values')
+                # data = np.array([p, n, kappa, pce_model.score, pce_model.cv_error])
+                # data = np.append(data, test_mse)
+                # data = np.append(data, test_mean_mse)
+                # data = np.append(data, test_std_mse)
+                # for iexcd_val, iexcd_mean_val in zip(excd_vals, excd_mean_val):
+                    # data = np.append(data, iexcd_val)
+                    # data = np.append(data, iexcd_mean_val)
+                # data_poly_deg.append(data)
 
-                outlist = ['polydegree', 'nsamples', 'kappa', 'score', 'cv_error']
-                for i in range(len(test_mse)):
-                    outlist.append('y_nst{:d}'.format(i))
+                # outlist = ['polydegree', 'nsamples', 'kappa', 'score', 'cv_error']
+                # for i in range(len(test_mse)):
+                    # outlist.append('y_nst{:d}'.format(i))
 
-                outlist.append('y_mean')
-                outlist.append('y_std')
-                npf, n = excd_vals.shape
-                for ipf in [4,5,6]:
-                    for i in range(n):
-                        outlist.append('y_pf{:d}_nst{:d}'.format(ipf, i))
-                        if i == n-1:
-                            outlist.append('y_mean_pf{:d}'.format(ipf))
-                outlist = [': '.join([str(str1),str2]) for str1, str2 in zip(range(np.size(data)), outlist)]
+                # outlist.append('y_mean')
+                # outlist.append('y_std')
+                # npf, nst = excd_vals.shape
+                # for ipf in [4,5,6]:
+                    # for i in range(nst):
+                        # outlist.append('y_pf{:d}_nst{:d}'.format(ipf, i))
+                        # if i == nst-1:
+                            # outlist.append('y_mean_pf{:d}'.format(ipf))
+                # outlist = [': '.join([str(str1),str2]) for str1, str2 in zip(range(np.size(data)), outlist)]
 
-                ### ============ calculating & updating metrics ============
-                tqdm.write(' > Summary')
-                with np.printoptions(precision=4):
-                    tqdm.write('     - {:<15s} : {}'.format( 'excd_vals'       , excd_mean_val))
-                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'Test MSE ' , test_mean_mse))
-                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'CV MSE'    , pce_model.cv_error))
-                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'Score '    , pce_model.score))
-                    tqdm.write('     - {:<15s} : {:.4f}'.format( 'kappa '    , kappa))
-                    tqdm.write('     ----------------------------------------')
+                # ### ============ calculating & updating metrics ============
+                # tqdm.write(' > Summary')
+                # with np.printoptions(precision=4):
+                    # tqdm.write('     - {:<15s} : {}'.format( 'excd_vals'       , excd_mean_val))
+                    # tqdm.write('     - {:<15s} : {:.4f}'.format( 'Test MSE ' , test_mean_mse))
+                    # tqdm.write('     - {:<15s} : {:.4f}'.format( 'CV MSE'    , pce_model.cv_error))
+                    # tqdm.write('     - {:<15s} : {:.4f}'.format( 'Score '    , pce_model.score))
+                    # tqdm.write('     - {:<15s} : {:.4f}'.format( 'kappa '    , kappa))
+                    # tqdm.write('     ----------------------------------------')
 
-    filename = '{:s}_{:s}_{:s}_reference'.format(solver.nickname, pce_model.tag, simparams.tag)
-    try:
-        np.save(os.path.join(simparams.data_dir_result, filename), np.array(data_poly_deg))
-        np.savetxt(os.path.join(simparams.data_dir_result, 'outlist.txt'), outlist, delimiter=' ', fmt='%s') 
-    except:
-        np.save(os.path.join(os.getcwd(), filename), np.array(data_poly_deg))
-        np.savetxt(os.path.join(simparams.data_dir_result, 'outlist.txt'), outlist, delimiter=' ', fmt='%s') 
+    # filename = '{:s}_{:s}_{:s}'.format(solver.nickname, pce_model.tag, simparams.tag)
+    # try:
+        # # np.save(os.path.join(simparams.data_dir_result, filename), np.array(data_poly_deg))
+        # # np.savetxt(os.path.join(simparams.data_dir_result, 'outlist.txt'), outlist, delimiter=' ', fmt='%s') 
+    # except:
+        # # np.save(os.path.join(os.getcwd(), filename), np.array(data_poly_deg))
+        # # np.savetxt(os.path.join(simparams.data_dir_result, 'outlist.txt'), outlist, delimiter=' ', fmt='%s') 
+        # np.save(os.path.join(os.getcwd(), filename+'_pred'), y_test_hat)
 
 
 if __name__ == '__main__':
