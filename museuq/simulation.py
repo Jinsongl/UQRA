9bf854d18981ded1812b76bff2d1629c4a0f6365
Jinsong <jinsongliu@utexas.edu>
2020-05-26 21:19:48 -0500

    if solver.name.lower() == 'linear oscillator', using full name  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   simulation.py

--- museuq/simulation.py
+++ museuq/simulation.py
@@ -211,7 +211,7 @@ class Modeling(object):
                 raise ValueError('Solver output format not understood: {}, expecting has {:d} in 1 dimensino'.format(n))
             elif y_test.ndim == 3:
                 ### (n_short_term, n, nqoi)
-                if solver.nickname.lower() == 'sdof':
+                if solver.name.lower() == 'linear oscillator':
                     y = []
                     for i, iqoi_test in enumerate(y_test.T):
                         data = np.vstack((u_test, x_test, iqoi_test.T))
@@ -220,7 +220,7 @@ class Modeling(object):
                             y.append(iqoi_test[:n].T if n > 0 else iqoi_test)
                     print('   > Saving test data to {} '.format(data_dir_result))
                     y_test = y[0] if len(y) == 1 else y
-                elif solver.nickname.lower() == 'duffing':
+                elif solver.name.lower() == 'duffing oscillator':
                     y = []
                     for i, iqoi_test in enumerate(y_test.T):
                         data = np.vstack((u_test, x_test, iqoi_test.T))
