4360b9efd894f7709c4263b086cd3cd7e8de67c7
Jinsong <jinsongliu@utexas.edu>
2020-08-06 13:42:27 -0500

    _experimentbase is outdated
    cleaned and updated
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   _experimentbase.py

--- uqra/experiment/_experimentbase.py
+++ uqra/experiment/_experimentbase.py
@@ -51,11 +51,7 @@ class ExperimentBase(object):
         else:
             raise ValueError('Sampling from type {} are not defined'.format(type(samplingfrom)))
 
-        self.x          = []  # DoE values in physical space 
-        self.u          = []  # DoE values in u-space (Askey)
-        self.filename   = 'DoE'
-
-    def samples(self, n_samples=None, theta=[0,1]):
+    def get_samples(self, n_samples=None, theta=[0,1]):
         """
         Return DoE samples based on specified DoE methods
 
@@ -64,32 +60,33 @@ class ExperimentBase(object):
         Returns:
             np.ndarray
         """
+        raise NotImplementedError
 
-        if self.samplingfrom is None:
-            pass 
-            ### Optimal design doesn't have to specify samplingfrom, use generated candidates
+        # if self.samplingfrom is None:
+            # pass 
+            # ### Optimal design doesn't have to specify samplingfrom, use generated candidates
 
-        else:
-            self.loc   = [0,] * self.ndim
-            self.scale = [1,] * self.ndim
-            ## possible theta input formats:
-            ## 1. [0,1] ndim == 1
-            ## 2. [0,1] ndim == n
-            ## 3. [[0,1],] ndim == 1
-            ## 4. [[0,1],] ndim == n
-            ## 5. [[0,1],[0,1],...] ndim == n
+        # else:
+            # self.loc   = [0,] * self.ndim
+            # self.scale = [1,] * self.ndim
+            # ## possible theta input formats:
+            # ## 1. [0,1] ndim == 1
+            # ## 2. [0,1] ndim == n
+            # ## 3. [[0,1],] ndim == 1
+            # ## 4. [[0,1],] ndim == n
+            # ## 5. [[0,1],[0,1],...] ndim == n
 
-            ## case 1,2 -> case 3,5
-            if isinstance(theta, list) and np.ndim(theta[0]) == 0:
-                theta = [theta,] * self.ndim
-            # case :4
-            for i, itheta in enumerate(theta):
-                self.loc[i] = itheta[0]
-                self.scale[i] = itheta[1]
+            # ## case 1,2 -> case 3,5
+            # if isinstance(theta, list) and np.ndim(theta[0]) == 0:
+                # theta = [theta,] * self.ndim
+            # # case :4
+            # for i, itheta in enumerate(theta):
+                # self.loc[i] = itheta[0]
+                # self.scale[i] = itheta[1]
 
-        ## updating filename 
-        self.n_samples= 0 if n_samples is None else int(n_samples)
-        self.filename = self.filename + num2print(n_samples)
+        # ## updating filename 
+        # self.n_samples= 0 if n_samples is None else int(n_samples)
+        # self.filename = self.filename + num2print(n_samples)
 
     def adaptive(self):
         """
