f57d89977e6a01e9ad4235341ff72db6899fcf4f
Jinsong <jinsongliu@utexas.edu>
2020-08-14 10:18:10 -0500

    update for HyeongUk comparison  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   ../examples/FPSO/nonadaptive_PCE.py

--- examples/FPSO/nonadaptive_PCE.py
+++ examples/FPSO/nonadaptive_PCE.py
@@ -48,10 +48,10 @@ def main(ST):
     np.set_printoptions(precision=4)
     np.set_printoptions(threshold=8)
     np.set_printoptions(suppress=True)
-    pf = 0.5/(50*365.25*24)
-    radius_surrogate= 3
+    pf = 1e-5 #0.5/(50*365.25*24)
+    radius_surrogate= 5
     Kvitebjorn      = uqra.environment.Kvitebjorn()
-    short_term_seeds_applied = np.setdiff1d(np.arange(11), np.array([]))
+    short_term_seeds_applied = np.setdiff1d(np.arange(10), np.array([]))
 
     ## ------------------------ Simulation Parameters ----------------- ###
     short_term_seeds = np.arange(ST,ST+1)
@@ -59,11 +59,12 @@ def main(ST):
     solver    = uqra.FPSO(phase=short_term_seeds)
     simparams = uqra.Parameters()
     simparams.solver     = solver
-    simparams.pce_degs   = np.array(range(2,21))
+    simparams.pce_degs   = np.array(range(2,11))
     simparams.n_cand     = int(1e5)
     simparams.n_test     = -1
+    simparams.n_pred     = int(1e5)
     simparams.doe_method = 'MCS' ### 'mcs', 'cls1', 'cls2', ..., 'cls5', 'reference'
-    simparams.optimality = 'D'# 'D', 'S', None
+    simparams.optimality = None # 'D', 'S', None
     simparams.poly_type  = 'heme'
     # simparams.poly_type  = 'hem'
     if simparams.poly_type.lower().startswith('hem'):
@@ -74,21 +75,22 @@ def main(ST):
         else:
             raise ValueError
 
-    simparams.fit_method = 'LASSOLARS'
+    simparams.fit_method = 'OLS'
     simparams.n_splits   = 50
     repeats              = 1 #if simparams.optimality is None else 1
-    alphas               = 1.2
+    alphas               = 10
     # # simparams.num_samples=np.arange(21+1, 130, 5)
     simparams.update()
     simparams.info()
 
-    ## ------------------------ MCS Benchmark ----------------- ###
-    print('------------------------------------------------------------')
-    print('>>> Monte Carlo Sampling for Model: FPSO                    ')
-    print('------------------------------------------------------------')
-    filename = 'DoE_McsE7R0.npy'
+    # ## ------------------------ MCS Benchmark ----------------- ###
+    # print('------------------------------------------------------------')
+    # print('>>> Monte Carlo Sampling for Model: FPSO                    ')
+    # print('------------------------------------------------------------')
+    filename = 'FPSO_SDOF_DoE_McsE7R0.npy'
     mcs_data = np.load(os.path.join(simparams.data_dir_result, filename))
-    mcs_data_ux, mcs_data_y = mcs_data[:4], mcs_data[4+short_term_seeds_applied,:]
+    # mcs_data_ux, mcs_data_y = mcs_data[:4], mcs_data[4+short_term_seeds_applied,:]
+    mcs_data_ux, mcs_data_y = mcs_data[:4], mcs_data[4:,:]
     y50_MCS  = uqra.metrics.mquantiles(mcs_data_y.T, 1-pf, multioutput='raw_values')
     y50_MCS_mean  = np.mean(y50_MCS)
     y50_MCS_std   = np.std(y50_MCS)
@@ -124,72 +126,52 @@ def main(ST):
     print('   - {:<25s} : {}'.format('y0', np.array(y50_EC[-1])))
     print('   - {:<25s} : {}'.format('Design state (u,x)', y50_EC[:4]))
 
-    np.random.seed(100)
-    EC2D_y_boots      = uqra.bootstrapping(EC2D_data_y, 100) 
-    EC2D_boots_median = np.median(EC2D_y_boots, axis=1)
-    y50_EC_boots_idx  = np.argmax(EC2D_boots_median, axis=-1)
-    y50_EC_boots_ux   = np.array([EC2D_data_ux[:,i] for i in y50_EC_boots_idx]).T
-    y50_EC_boots_y    = np.max(EC2D_boots_median,axis=-1) 
-    y50_EC_boots      = np.concatenate((y50_EC_boots_ux, y50_EC_boots_y.reshape(1,-1)), axis=0)
-    y50_EC_boots_mean = np.mean(y50_EC_boots, axis=1)
-    y50_EC_boots_std  = np.std(y50_EC_boots, axis=1)
-    print(' > Extreme reponse from EC (Bootstrap (n={:d})):'.format(EC2D_y_boots.shape[0]))
-    print('   - {:<25s} : {}'.format('Bootstrap data set', EC2D_y_boots.shape))
-    print('   - {:<25s} : [{:.2f}, {:.2f}]'.format('y50[mean, std]',y50_EC_boots_mean[-1], y50_EC_boots_std[-1]))
-    print('   - {:<25s} : {}'.format('Design state (u,x)', y50_EC_boots_mean[:4]))
+    # np.random.seed(100)
+    # EC2D_y_boots      = uqra.bootstrapping(EC2D_data_y, 100) 
+    # EC2D_boots_median = np.median(EC2D_y_boots, axis=1)
+    # y50_EC_boots_idx  = np.argmax(EC2D_boots_median, axis=-1)
+    # y50_EC_boots_ux   = np.array([EC2D_data_ux[:,i] for i in y50_EC_boots_idx]).T
+    # y50_EC_boots_y    = np.max(EC2D_boots_median,axis=-1) 
+    # y50_EC_boots      = np.concatenate((y50_EC_boots_ux, y50_EC_boots_y.reshape(1,-1)), axis=0)
+    # y50_EC_boots_mean = np.mean(y50_EC_boots, axis=1)
+    # y50_EC_boots_std  = np.std(y50_EC_boots, axis=1)
+    # print(' > Extreme reponse from EC (Bootstrap (n={:d})):'.format(EC2D_y_boots.shape[0]))
+    # print('   - {:<25s} : {}'.format('Bootstrap data set', EC2D_y_boots.shape))
+    # print('   - {:<25s} : [{:.2f}, {:.2f}]'.format('y50[mean, std]',y50_EC_boots_mean[-1], y50_EC_boots_std[-1]))
+    # print('   - {:<25s} : {}'.format('Design state (u,x)', y50_EC_boots_mean[:4]))
 
-    u_center = y50_EC_boots_mean[ :2].reshape(-1, 1)
-    x_center = y50_EC_boots_mean[2:4].reshape(-1, 1)
-    print(' > Important Region based on EC(boots):')
-    print('   - {:<25s} : {}'.format('Radius', radius_surrogate))
-    print('   - {:<25s} : {}'.format('Center U', np.squeeze(u_center)))
-    print('   - {:<25s} : {}'.format('Center X', np.squeeze(x_center)))
-    print('================================================================================')
-
-
-    ## ----------- Candidate and testing data set for DoE ----------- ###
-    ## ----- validation data set centered around y50_EC
-    # print(' > Getting validation data set...')
-    # filename_validate  = 'FPSO_DoE_{:s}E5R0.npy'.format(simparams.doe_method.capitalize())
-    # data_vald = np.load(os.path.join(simparams.data_dir_result, filename_validate)) 
-    # u_vald    = data_vald[             :  solver.ndim]
-    # x_vald    = data_vald[solver.ndim  :2*solver.ndim]
-    # y_vald    = np.squeeze(data_vald[2*solver.ndim+short_term_seeds,:])
-    # # y_vald    = np.mean(y_vald, axis=0).reshape(1,-1)
-    # print('   - {:<25s} : {}, {}, {}'.format('Validation Dataset (U,X,Y)',u_vald.shape, x_vald.shape, y_vald.shape ))
-    # print('   - {:<25s} : [{}, {}]'.format('Validation U[mean, std]',np.mean(u_vald, axis=1),np.std (u_vald, axis=1)))
-    # print('   - {:<25s} : [{}]'.format('Validation max(U)[U1, U2]',np.amax(abs(u_vald), axis=1)))
-    # print('   - {:<25s} : [{}]'.format('Validatoin [min(Y), max(Y)]',np.array([np.amin(y_vald),np.amax(y_vald)])))
+    # u_center = y50_EC_boots_mean[ :2].reshape(-1, 1)
+    # x_center = y50_EC_boots_mean[2:4].reshape(-1, 1)
+    # print(' > Important Region based on EC(boots):')
+    # print('   - {:<25s} : {}'.format('Radius', radius_surrogate))
+    # print('   - {:<25s} : {}'.format('Center U', np.squeeze(u_center)))
+    # print('   - {:<25s} : {}'.format('Center X', np.squeeze(x_center)))
+    # print('================================================================================')
+    u_center = np.array([0,0]).reshape(-1,1)
+    x_center = np.array([0,0]).reshape(-1,1)
 
 
-    ## ----- Testing data set centered around (0,0)
-    print(' > Getting Testing data set...')
+    ## ----------- Test data set ----------- ###
+    ## ----- Testing data set centered around u_center, first 100000
+    print(' > Getting Test data set...')
     filename    = 'FPSO_Test_McsE7R0.npy'
     data_test   = np.load(os.path.join(simparams.data_dir_result, filename))
     u_test      = data_test[:solver.ndim, :]
     x_test      = data_test[solver.ndim:2*solver.ndim, :]
     y_test      = np.squeeze(data_test[2*solver.ndim+short_term_seeds,:])
 
-    u_pred      = mcs_data_ux[ :2,np.linalg.norm(mcs_data_ux[:2] - u_center, axis=0) < radius_surrogate]
-    x_pred      = mcs_data_ux[-2:,np.linalg.norm(mcs_data_ux[:2] - u_center, axis=0) < radius_surrogate]
-
-    # filename    = 'DoE_McsE7R0.npy'
-    # u_test      = np.load(os.path.join(simparams.data_dir_sample, 'MCS', 'Norm', filename))[:solver.ndim,:]
-    # x_test      = Kvitebjorn.ppf(stats.norm().cdf(u_test))
-    # y_test      = -np.inf * np.ones((u_test.shape[1],))
-    # idx_in_circle   = np.arange(u_test.shape[1])[np.linalg.norm(u_test-u_center, axis=0) < radius_surrogate]
-    # u_test_in_circle= u_test[:,idx_in_circle]
-    # x_test_in_circle= x_test[:,idx_in_circle]
-    # y_test_in_circle= solver.run(x_test_in_circle, verbose=True)
-    # np.save(os.path.join(simparams.data_dir_result, 'y_test_in_circle.npy'), y_test_in_circle)
-
     print('   - {:<25s} : {}, {}, {}'.format('Test Dataset (U,X,Y)',u_test.shape, x_test.shape, y_test.shape ))
     # print('   - {:<25s} : [{}, {}]'.format('Test U[mean, std]',np.mean(u_test, axis=1),np.std (u_test, axis=1)))
     print('   - {:<25s} : [{}]'.format('Test max(U)[U1, U2]',np.amax(abs(u_test), axis=1)))
     print('   - {:<25s} : [{}]'.format('Test [min(Y), max(Y)]',np.array([np.amin(y_test),np.amax(y_test)])))
-    print('   - {:<25s} : {}, {}'.format('Prediction Dataset (U,X)',u_pred.shape, x_pred.shape))
+    # print('   - {:<25s} : {}, {}'.format('Prediction Dataset (U,X)',u_pred.shape, x_pred.shape))
     # print('   - {:<25s} : {}'.format('# Test data in Circle({:.2f})'.format(radius_surrogate),u_test_in_circle.shape))
 
+
+    ## ----------- Predict data set ----------- ###
+    ## ----- Prediction data set centered around u_center, all  
+    # u_pred      = mcs_data_ux[ :2,np.linalg.norm(mcs_data_ux[:2] - u_center, axis=0) < radius_surrogate]
+    # x_pred      = mcs_data_ux[-2:,np.linalg.norm(mcs_data_ux[:2] - u_center, axis=0) < radius_surrogate]
 
     ## ----------- Candidate and testing data set for DoE ----------- ###
     print(' > Getting candidate data set...')
@@ -207,7 +189,7 @@ def main(ST):
     ### ============ Initial Values ============
 
     res_pce_deg = []
-    data_test = []
+    data_pred = []
 
     for pce_deg in simparams.pce_degs:
         print('\n================================================================================')
@@ -266,18 +248,28 @@ def main(ST):
         print('   - {:<25s} : [{}]'.format('Train [min(Y), max(Y)]',np.array([np.amin(y_train),np.amax(y_train)])))
 
         y_train_hat = pce_model.predict(u_train)
-        # y_vald_hat  = pce_model.predict(u_vald-u_center)
-        # vald_mse    = uqra.metrics.mean_squared_error(y_vald.T, y_vald_hat.T,multioutput='raw_values')
 
         y_test_hat = pce_model.predict(u_test - u_center)
         test_error = uqra.metrics.mean_squared_error(y_test, y_test_hat,multioutput='raw_values')
 
+
+        ### prediction data set, randomly draw or from MCS directory
+        # y_pred = pce_model.predict(u_pred - u_center)
+        # alpha  = (pf * mcs_data_ux.shape[1]) / y_pred.size
+        # y50_pce_y   = uqra.metrics.mquantiles(y_pred, 1-alpha)
+        # y50_pce_idx = np.array(abs(y_pred - y50_pce_y)).argmin()
+        # y50_pce_uxy = np.concatenate((u_pred[:,y50_pce_idx], x_pred[:, y50_pce_idx], y50_pce_y)) 
+        # data_pred.append([pce_deg, n_train, y_pred])
+
+        np.random.seed()
+        u_pred = stats.norm.rvs(size=(solver.ndim,simparams.n_pred))
+        x_pred = Kvitebjorn.ppf(stats.norm.cdf(u_pred))
         y_pred = pce_model.predict(u_pred - u_center)
-        alpha  = (pf * mcs_data_ux.shape[1]) / y_pred.size
-        y50_pce_y   = uqra.metrics.mquantiles(y_pred, 1-alpha)
+        y50_pce_y   = uqra.metrics.mquantiles(y_pred, 1-pf)
         y50_pce_idx = np.array(abs(y_pred - y50_pce_y)).argmin()
         y50_pce_uxy = np.concatenate((u_pred[:,y50_pce_idx], x_pred[:, y50_pce_idx], y50_pce_y)) 
-        data_test.append([pce_deg, n_train, y_pred])
+        data_pred.append([pce_deg, n_train, y_pred])
+
         res = [pce_deg, n_train, pce_model.cv_error, test_error[0]]
         for item in y50_pce_uxy:
             res.append(item)
@@ -294,22 +286,11 @@ def main(ST):
             # tqdm.write('     - {:<15s} : {:.4f}'.format( 'kappa '    , kappa))
 
 
+    ### ============ Saving QoIs ============
     res_pce_deg = np.array(res_pce_deg)
-    data_test = np.array(data_test, dtype=object)
-
     with open(os.path.join(simparams.data_dir_result, 'outlist_name.txt'), "w") as text_file:
         text_file.write('\n'.join(['pce_deg', 'n_train', 'cv_error', 'test mse', 'y50_pce_u', 'y50_pce_x', 'y50_pce_y']))
 
-    ### Saving test data in circle
-    filename = '{:s}_{:s}_{:s}_Alpha{}_ST{}_test'.format(solver.nickname, pce_model.tag, 
-            simparams.tag, str(alphas).replace('.', 'pt'), short_term_seeds[-1])
-    try:
-        np.save(os.path.join(simparams.data_dir_result, filename), data_test)
-    except:
-        print(' Directory not found: {}, file save locally... '.format(simparams.data_dir_result))
-        np.save(os.path.join(os.getcwd(), filename), data_test)
-
-    ### Saving QoI data  
     filename = '{:s}_{:s}_{:s}_Alpha{}_ST{}'.format(solver.nickname, pce_model.tag, 
             simparams.tag, str(alphas).replace('.', 'pt'), short_term_seeds[-1])
     try:
@@ -319,6 +300,16 @@ def main(ST):
         np.save(os.path.join(os.getcwd(), filename), res_pce_deg)
 
 
+    ### ============ Saving Predict data ============
+    data_pred = np.array(data_pred, dtype=object)
+    filename = '{:s}_{:s}_{:s}_Alpha{}_ST{}_pred'.format(solver.nickname, pce_model.tag, 
+            simparams.tag, str(alphas).replace('.', 'pt'), short_term_seeds[-1])
+    try:
+        np.save(os.path.join(simparams.data_dir_result, filename), data_pred)
+    except:
+        print(' Directory not found: {}, file save locally... '.format(simparams.data_dir_result))
+        np.save(os.path.join(os.getcwd(), filename), data_pred)
+
 if __name__ == '__main__':
-    for s in range(11):
+    for s in range(3,10):
         main(s)
