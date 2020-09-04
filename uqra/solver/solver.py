ad887d08fa90fa8e0eeca34cbf7358895b97863c
Jinsong <jinsongliu@utexas.edu>
2020-09-04 12:16:53 -0500

    empty Solver class  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   solver/solver.py

---
+++ uqra/solver/solver.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Jinsong Liu <jinsongliu@utexas.edu>
+#
+# Distributed under terms of the GNU-License license.
+
+"""
+Single degree of freedom with time series external loads
+"""
+
+import uqra
+from uqra.solver._solverbase import SolverBase
+import os, numpy as np, scipy as sp
+# from scipy.integrate import odeint
+# from scipy.optimize import brentq
+from .PowerSpectrum import PowerSpectrum
+from uqra.environment import Kvitebjorn 
+from tqdm import tqdm
+import scipy.stats as stats
+
+
+class Solver(SolverBase):
+    """
+
+    """
+    def __init__(self, name, ndim):
+        """
+        Empty Solver object
+        """
+        super().__init__()
+        self.name       = name 
+        self.nickname   = name
+        self.ndim       = int(ndim)
+
+    def __str__(self):
+        message = '{:s} (ndim={:d})'.format(self.name, self.ndim)
+        return message
+
+    def run(self):
+        raise NotImplementedError
+
+            
+    def map_domain(self):
+        raise NotImplementedError
+
+
+
+
